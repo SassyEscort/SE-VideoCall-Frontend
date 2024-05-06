af58dafaafe70cac50d44f80d2d55f2555274fab
dhruv-divtech <dhruv.divtech@gmail.com>
2024-05-06 12:10:34 +0530

    home page faq

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -1,18 +1,21 @@
 'use client';
 
-import UIThemeButton from 'components/UIComponents/UIStyledLoadingButton';
-import { FormattedMessage } from 'react-intl';
-import { toast } from 'react-toastify';
+import HomePageFAQ from 'views/home/HomePageFAQ/HomePageFAQ';
+
+// import UIThemeButton from 'components/UIComponents/UIStyledLoadingButton';
+// import { FormattedMessage } from 'react-intl';
+// import { toast } from 'react-toastify';
 
 const HomeContainer = () => {
-  const handle = () => {
-    toast.success('flirtbate');
-  };
+  // const handle = () => {
+  //   toast.success('flirtbate');
+  // };
 
   return (
-    <UIThemeButton variant="contained" onClick={handle}>
-      <FormattedMessage id="flirtbate" />
-    </UIThemeButton>
+    // <UIThemeButton variant="contained" onClick={handle}>
+    //   <FormattedMessage id="flirtbate" />
+    // </UIThemeButton>
+    <HomePageFAQ />
   );
 };
 
