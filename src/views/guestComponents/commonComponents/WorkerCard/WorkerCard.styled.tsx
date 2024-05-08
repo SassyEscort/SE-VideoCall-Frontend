5970eb909f8ec61267103276abc43a5638a978e8
dhruv-divtech <dhruv.divtech@gmail.com>
2024-05-08 11:25:13 +0530

    comment start video call button

--- src/views/guestComponents/commonComponents/WorkerCard/WorkerCard.styled.tsx
+++ src/views/guestComponents/commonComponents/WorkerCard/WorkerCard.styled.tsx
@@ -87,8 +87,8 @@ export const SeconderContainerWorkerCard = styled(Box)(({ theme }) => ({
   [theme.breakpoints.only('sm')]: {
     padding: theme.spacing(1.5)
   },
-  padding: theme.spacing(2),
-  background: 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.88) 100%)'
+  padding: theme.spacing(2)
+  // background: 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.88) 100%)'
 }));
 
 export const SubContainertWorkerCard = styled(Box)(() => ({
