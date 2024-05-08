cf89a1625ef0c9f0ff3c234495474f177fcc2772
dhruv-divtech <dhruv.divtech@gmail.com>
2024-05-08 19:01:54 +0530

    working on details page

---
+++ src/hooks/useImageOptimize.tsx
@@ -0,0 +1,50 @@
+import { MutableRefObject, useEffect, useState } from 'react';
+
+function useImageOptimize(
+  imageUrlRef: MutableRefObject<HTMLElement | undefined>,
+  photo: string,
+  type?: 'BG' | 'IMG',
+  noResize?: boolean,
+  isWaterMark?: boolean,
+  coordinates?: string
+) {
+  type = type ?? 'BG';
+  isWaterMark = isWaterMark ?? true;
+
+  const [imageUrl, setImageUrl] = useState('');
+
+  useEffect(() => {
+    if (imageUrlRef.current && photo) {
+      const height = imageUrlRef.current.clientHeight;
+      // const videoTypeCondition = VideoAcceptType.includes(photo.substring(photo.lastIndexOf('.') + 1));
+
+      setImageUrl(
+        photo.split('/images')[0] + `/images/` + (coordinates ? '' : !noResize ? `:h-${height + 100}` : '') + photo.split('/images')[1]
+      );
+    }
+  }, [coordinates, imageUrlRef, isWaterMark, noResize, photo]);
+
+  useEffect(() => {
+    const setImage = (image: string) => {
+      if (imageUrlRef.current && image) {
+        if (type === 'BG') imageUrlRef.current.style.backgroundImage = 'url(' + image + ')';
+        else imageUrlRef.current.setAttribute('src', image);
+      }
+    };
+
+    setImage(imageUrl);
+
+    // const finalCroppedImage = async () => {
+    //   const existingCords: Area | undefined = coordinates && JSON.parse(coordinates).cords;
+
+    //   if (existingCords) {
+    //     const croppedImage = `${photo}?tr=w-${existingCords.width},h-${existingCords.height},cm-extract,x-${existingCords.x},y-${existingCords.y}`;
+    //     setImage(croppedImage);
+    //   } else setImage(imageUrl);
+    // };
+
+    // finalCroppedImage();
+  }, [coordinates, imageUrl, imageUrlRef, photo, type]);
+}
+
+export default useImageOptimize;
---
+++ src/views/guestComponents/details/EscortDetailPage/Escort.styled.tsx
@@ -0,0 +1,10 @@
+import { styled } from '@mui/material';
+import Box from '@mui/material/Box';
+
+export const DetailsMainBox = styled(Box)(() => ({
+  width: '100%',
+  maxWidth: '517px',
+  backgroundColor: '#1E0815',
+  padding: '16px 32px 16px 32px',
+  borderRadius: '12px'
+}));
