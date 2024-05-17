61f50f90d3fe0cd65ffbfd31675e45149f8958bc
dhruv-divtech <dhruv.divtech@gmail.com>
2024-05-17 11:49:12 +0530

    home page select menu changes done

--- src/views/guestViews/searchPage/searchFilters/Price.tsx
+++ src/views/guestViews/searchPage/searchFilters/Price.tsx
@@ -3,18 +3,18 @@ import FormControl from '@mui/material/FormControl';
 import MenuItem from '@mui/material/MenuItem';
 import UINewTypography from 'components/UIComponents/UINewTypography';
 import { StyledSelectInputLabel, UIStyledSelect } from 'components/UIComponents/UIStyledSelect';
-import { AGES } from 'constants/searchConstants';
+import { PRICE } from 'constants/searchConstants';
 
-const AgeFilter = () => {
+const Price = () => {
   return (
-    <FormControl id="age" sx={{ width: '100%', maxWidth: { lg: '203px', sm: '235px' } }}>
-      <StyledSelectInputLabel>Age Range</StyledSelectInputLabel>
-      <UIStyledSelect MenuProps={{ disableScrollLock: true }} label="age range" name="age" labelId="age" IconComponent={ExpandMore}>
-        {AGES.map((age, key: number) => {
+    <FormControl id="price" fullWidth sx={{ display: 'flex', width: '100%', maxWidth: { lg: '203px', sm: '235px' } }}>
+      <StyledSelectInputLabel>Price</StyledSelectInputLabel>
+      <UIStyledSelect MenuProps={{ disableScrollLock: true }} label="price" name="price" labelId="price" IconComponent={ExpandMore}>
+        {PRICE.map((price, key: number) => {
           return (
-            <MenuItem key={key} value={age.id}>
+            <MenuItem key={key} value={price.id}>
               <UINewTypography variant="buttonLargeMenu" color="text.secondary">
-                {age.title}
+                {price.title}
               </UINewTypography>
             </MenuItem>
           );
@@ -24,4 +24,4 @@ const AgeFilter = () => {
   );
 };
 
-export default AgeFilter;
+export default Price;
