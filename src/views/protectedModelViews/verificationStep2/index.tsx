86d149452dbfc6beef181ff4c6ed53099e84abf1
muskanlalani123 <mukan.divtech@gmail.com>
2024-05-20 15:28:29 +0530

    varifation step 2 ui done and formik use and validation set

--- src/views/protectedModelViews/verificationStep2/index.tsx
+++ src/views/protectedModelViews/verificationStep2/index.tsx
@@ -31,9 +31,7 @@ const VerificationStep2 = () => {
         idNumber: ''
       }}
       validationSchema={validationVerificationSchema}
-      onSubmit={(values) => {
-        // console.log('hello', values);
-      }}
+      onSubmit={(values) => {}}
     >
       {({ values, errors, touched, handleChange, handleBlur, handleSubmit }) => {
         return (
