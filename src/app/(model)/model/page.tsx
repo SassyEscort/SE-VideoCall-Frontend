e226772a12da3e5ffedff8b639b786d24affdb7d
muskanlalani123 <mukan.divtech@gmail.com>
2024-05-21 17:34:48 +0530

    main page remove verification step2

--- src/app/(model)/model/page.tsx
+++ src/app/(model)/model/page.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import HomeContainerModel from 'views/modelViews/modelPage';
-import VerificationStep2 from 'views/protectedModelViews/verificationStep2';
 
 const Home = () => {
-  return <VerificationStep2 />;
+  return <HomeContainerModel />;
 };
 
 export default Home;
