4e7d7be8701db70902dd7357ef98f12e7f4b6683
dhruv-divtech <dhruv.divtech@gmail.com>
2024-05-23 13:04:17 +0530

    drop-down menu

--- src/components/UIComponents/UIStyledNewFormLable/index.tsx
+++ src/components/UIComponents/UIStyledNewFormLable/index.tsx
@@ -7,7 +7,7 @@ export const UIStyledNewFormLableContainer = styled(FormControlLabel)(({ theme }
   m: 0,
   padding: '10px 12px',
   gap: '6px',
-  borderRadius: '8px',
+  borderRadius: theme.spacing(1),
   border: '1px solid',
   borderColor: theme.palette.primary[700],
   '& .MuiSvgIcon-root': {
