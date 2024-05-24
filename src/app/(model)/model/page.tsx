b8a0e1ac2b1ed64bdfb7b327c94476e8af9c78d8
muskanlalani123 <mukan.divtech@gmail.com>
2024-05-24 17:27:42 +0530

    price page create and payout page create ui

--- src/app/(model)/model/page.tsx
+++ src/app/(model)/model/page.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import HomeContainerModel from 'views/modelViews/modelPage';
+import Payment from 'views/protectedModelViews/verification/payment';
 
 const Home = () => {
-  return <HomeContainerModel />;
+  return <Payment />;
 };
 
 export default Home;
--- src/constants/workerVerification.ts
+++ src/constants/workerVerification.ts
@@ -7,8 +7,14 @@ export const GENDER: MultipleOptions[] = [
   { id: 'Female', name: 'Female' },
   { id: 'Trans', name: 'Trans' }
 ];
+
 interface VerificationStep {
   name: string;
 }
 
 export const VerificationStepSecond: VerificationStep[] = [{ name: 'ID Card' }, { name: 'Password' }, { name: `Driver's License` }];
+
+interface price {
+  price: number;
+}
+export const priceValueMenuItme: price[] = [{ price: 4.99 }, { price: 2.99 }, { price: 5.99 }];
