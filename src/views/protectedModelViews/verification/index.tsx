cbe4be8b00bfcd8b8ff5458c3f2b87709e151088
Muskan Lalani <mukan.divtech@gmail.com>
2024-05-28 13:14:58 +0530

    added thumbnail blob functionality

--- src/views/protectedModelViews/verification/index.tsx
+++ src/views/protectedModelViews/verification/index.tsx
@@ -188,6 +188,7 @@ const VerificationContainer = () => {
         <UploadImage
           workerPhotos={modelDetails?.photos ?? []}
           token={token}
+          handleModelApiChange={handleModelApiChange}
           handleNext={handleNext}
           handlePrevVerificationStep={handlePrev}
         />
