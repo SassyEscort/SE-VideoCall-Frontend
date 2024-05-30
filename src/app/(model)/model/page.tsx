d2f581865df2dea6004e44b634f714e2934418c8
muskanlalani123 <mukan.divtech@gmail.com>
2024-05-30 12:58:50 +0530

    ui create payment container list

--- src/app/(model)/model/page.tsx
+++ src/app/(model)/model/page.tsx
@@ -1,9 +1,8 @@
 import React from 'react';
 import HomeContainerModel from 'views/modelViews/modelPage';
-import PayoutPaymentConatiner from 'views/protectedDashboardViews/payoutPaymentContainer';
 
 const Home = () => {
-  return <PayoutPaymentConatiner />;
+  return <HomeContainerModel />;
 };
 
 export default Home;
