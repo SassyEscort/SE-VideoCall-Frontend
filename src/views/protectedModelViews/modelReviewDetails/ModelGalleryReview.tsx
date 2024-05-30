a2e702d6be7ec6e40fc686a8cf70850ee68b166d
Muskan Lalani <mukan.divtech@gmail.com>
2024-05-30 19:03:11 +0530

    added usememo

--- src/views/protectedModelViews/modelReviewDetails/ModelGalleryReview.tsx
+++ src/views/protectedModelViews/modelReviewDetails/ModelGalleryReview.tsx
@@ -4,40 +4,27 @@ import { FormattedMessage } from 'react-intl';
 import PreviewGallery from '../verification/stepThree/uploadImage/PreviewGallery';
 import { ModelDetailsResponse } from '../verification/verificationTypes';
 import { ModelGalleryTitleBox } from './ModelReviewDetails.styled';
-import { useMemo, useState } from 'react';
+import { useMemo } from 'react';
 import theme from 'themes/theme';
-import { UploadPhotos } from '../verification/stepThree/uploadImage/ModelMultiplePhoto';
 
 const ModelGalleryReview = ({ modelDetails }: { modelDetails: ModelDetailsResponse }) => {
   const isSmUp = useMediaQuery(theme.breakpoints.up('sm'));
   const height = isSmUp ? 193 : 210;
   const width = isSmUp ? 145 : 159;
 
-  const [existingPhotos, setExistingPhotos] = useState<UploadPhotos[]>([]);
-
-  useMemo(() => {
-    const existedPhoto = modelDetails?.photos
+  const existedPhoto = useMemo(() => {
+    return modelDetails?.photos
       ?.filter((photo) => !photo.is_document)
       ?.map((photo, index) => {
-        if (photo.type === 'file_5')
-          return {
-            id: photo.id,
-            name: `file5Existing[${index - 4}]`,
-            photoURL: photo.link,
-            cords: photo.cords
-          };
-        else {
-          return {
-            id: photo.id,
-            name: `file${photo?.type?.split('_')[1]}`,
-            photoURL: photo.link,
-            cords: photo.cords,
-            isFavorite: photo.favourite === 1
-          };
-        }
+        return {
+          id: photo.id,
+          name: `file5Existing[${index - 4}]`,
+          photoURL: photo.link,
+          cords: photo.cords,
+          isFavorite: photo.favourite === 1
+        };
       });
-    setExistingPhotos(existedPhoto);
-  }, [modelDetails?.photos]);
+  }, [modelDetails]);
 
   return (
     <ModelGalleryTitleBox>
@@ -46,7 +33,7 @@ const ModelGalleryReview = ({ modelDetails }: { modelDetails: ModelDetailsRespon
       </UINewTypography>
       <Box sx={{ display: 'flex', gap: 2, flexWrap: 'wrap' }}>
         {modelDetails?.photos?.length &&
-          existingPhotos?.map((photo, index) => {
+          existedPhoto?.map((photo, index) => {
             return <PreviewGallery key={index} image={photo} isEdit={false} height={height} width={width} />;
           })}
       </Box>
