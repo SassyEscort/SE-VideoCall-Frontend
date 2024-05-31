ffd95e80d245100c159dfed9d318a17e902df5a7
muskanlalani123 <mukan.divtech@gmail.com>
2024-05-31 15:08:35 +0530

    confilt resove

--- src/views/protectedDashboardViews/index.tsx
+++ src/views/protectedDashboardViews/index.tsx
@@ -4,11 +4,11 @@ import MainLayoutNav from './protectedDashboardLayout';
 import HomeMainModelContainer from 'views/modelViews/modelLayout/homeModelContainer';
 import { ModelDetailsResponse } from 'views/protectedModelViews/verification/verificationTypes';
 
-const DashboardProfile = ({ children, modelDetails }: { children: ReactNode; modelDetails: ModelDetailsResponse }) => {
+const DashboardProfile = ({ children, modelDetails }: { children: ReactNode; modelDetails?: ModelDetailsResponse }) => {
   return (
     <>
       <HomeMainModelContainer>
-        <MainLayoutNav variant={'worker'} enlargedFooter={true} modelDetails={modelDetails}>
+        <MainLayoutNav variant={'worker'} enlargedFooter={true} modelDetails={modelDetails ?? ({} as ModelDetailsResponse)}>
           {children}
         </MainLayoutNav>
       </HomeMainModelContainer>
