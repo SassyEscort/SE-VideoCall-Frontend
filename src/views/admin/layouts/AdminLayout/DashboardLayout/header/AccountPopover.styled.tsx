09ca4ba541aa13eba86a8f143eec017acdc1f83b
dhruv-divtech <dhruv.divtech@gmail.com>
2024-06-11 14:26:30 +0530

    admin page text changes

--- src/views/admin/layouts/AdminLayout/DashboardLayout/header/AccountPopover.styled.tsx
+++ src/views/admin/layouts/AdminLayout/DashboardLayout/header/AccountPopover.styled.tsx
@@ -1,4 +1,4 @@
-import { AppBar, IconButton, Toolbar, styled } from '@mui/material';
+import { AppBar, IconButton, Popover, Toolbar, styled } from '@mui/material';
 
 export const StyledIconButton = styled(IconButton)(({ open }: { open: boolean }) => ({
   p: 0,
@@ -23,5 +23,18 @@ export const StyledRoot = styled(AppBar)(({ theme }) => ({
 export const StyledToolbar = styled(Toolbar)(({ theme }) => ({
   [theme.breakpoints.up('lg')]: {
     padding: theme.spacing(0, 5)
+  }
+}));
+
+export const StyledPopover = styled(Popover)(({ theme }) => ({
+  '& .MuiPaper-root': {
+    padding: 0,
+    marginTop: theme.spacing(1.5),
+    marginLeft: theme.spacing(0.75),
+    width: 180
+  },
+  '& .MuiMenuItem-root': {
+    typography: 'body2',
+    borderRadius: 0.75
   }
 }));
--- src/views/admin/layouts/AdminLayout/DashboardLayout/header/AccountPopover.tsx
+++ src/views/admin/layouts/AdminLayout/DashboardLayout/header/AccountPopover.tsx
@@ -1,9 +1,8 @@
 'use client';
 import Avatar from '@mui/material/Avatar';
-import { StyledIconButton } from './AccountPopover.styled';
+import { StyledIconButton, StyledPopover } from './AccountPopover.styled';
 import { useState } from 'react';
 import { Divider, MenuItem, Stack } from '@mui/material';
-import Popover from '@mui/material/Popover';
 
 export default function AccountPopover() {
   const [open, setOpen] = useState(false);
@@ -13,29 +12,17 @@ export default function AccountPopover() {
       <StyledIconButton open={open} onClick={() => setOpen(!open)}>
         <Avatar src={'/images/admin/avatar.jpg'} alt="photoURL" />
       </StyledIconButton>
-      <Popover
+      <StyledPopover
         open={Boolean(open)}
         anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
         transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        PaperProps={{
-          sx: {
-            p: 0,
-            mt: 1.5,
-            ml: 0.75,
-            width: 180,
-            '& .MuiMenuItem-root': {
-              typography: 'body2',
-              borderRadius: 0.75
-            }
-          }
-        }}
       >
         <Stack sx={{ p: 1 }}>
           <MenuItem>Change Password</MenuItem>
         </Stack>
         <Divider sx={{ borderStyle: 'dashed' }} />
         <MenuItem sx={{ m: 1 }}>Logout</MenuItem>
-      </Popover>
+      </StyledPopover>
     </>
   );
 }
