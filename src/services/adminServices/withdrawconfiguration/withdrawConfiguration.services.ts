6f215d802e7fd0af1d0df54a6cb816a07ebaed9c
dhruv-divtech <dhruv.divtech@gmail.com>
2024-06-13 14:57:17 +0530

    camelcase

--- src/services/adminServices/withdrawconfiguration/withdrawConfiguration.services.ts
+++ src/services/adminServices/withdrawconfiguration/withdrawConfiguration.services.ts
@@ -4,7 +4,7 @@ export type withdrawParams = {
   withdrawal_amt: string;
 };
 
-export type withdrawresponse = {
+export type WithdrawResponse = {
   data: string | null;
   code: number;
   error: string | null;
@@ -12,9 +12,9 @@ export type withdrawresponse = {
 };
 
 export class withdrawMinAmountServices {
-  static withdrawMinAmount = async (params: withdrawParams, token: string): Promise<withdrawresponse> => {
+  static withdrawMinAmount = async (params: withdrawParams, token: string): Promise<WithdrawResponse> => {
     try {
-      const res = await axios.post<withdrawresponse>(`${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/admin/model/withdrawal-amount`, params, {
+      const res = await axios.post<WithdrawResponse>(`${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/admin/model/withdrawal-amount`, params, {
         headers: {
           'Content-Type': 'application/json',
           Authorization: token
@@ -23,7 +23,7 @@ export class withdrawMinAmountServices {
       return res.data;
     } catch (err: any) {
       const error: AxiosError = err;
-      return error.response?.data as withdrawresponse;
+      return error.response?.data as WithdrawResponse;
     }
   };
 }
