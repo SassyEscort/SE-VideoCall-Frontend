6cafae08557182a30c56980056a86501c93b2549
muskanlalani123 <mukan.divtech@gmail.com>
2024-06-20 15:25:41 +0530

    change calculateAge function code

--- src/constants/adminAgeCalculate.tsx
+++ src/constants/adminAgeCalculate.tsx
@@ -1,14 +1,9 @@
+import moment from 'moment';
+
 export const calculateAge = (dob: string) => {
   if (!dob) {
     return '';
   }
-  const birthDate = new Date(dob);
-  const today = new Date();
-  let age = today.getFullYear() - birthDate.getFullYear();
-  const monthDifference = today.getMonth() - birthDate.getMonth();
-  const dayDifference = today.getDate() - birthDate.getDate();
-  if (monthDifference < 0 || (monthDifference === 0 && dayDifference < 0)) {
-    age--;
-  }
-  return age;
+  const calculatedAge = moment().diff(moment(dob), 'years');
+  return calculatedAge;
 };
