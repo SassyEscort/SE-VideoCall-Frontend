d31c6f1bb3149a76fbda866680c40aec65afce8a
dhruv-divtech <dhruv.divtech@gmail.com>
2024-07-02 15:56:47 +0530

    callhistory page api calling done

---
+++ src/services/callHistory/types.ts
@@ -0,0 +1,41 @@
+import { GenericRes } from 'services/guestAuth/authuser.services';
+
+export type CallHistoryDetails = {
+  model_id: number;
+  status: string;
+  start_time: string;
+  end_time: string;
+  duration: string;
+  call_type: string;
+  credits_used: number;
+  name: string;
+  dob: string;
+  price_per_minute: number;
+  link: null | string;
+  languages: CallHistoryLanguages[];
+};
+
+export type CallHistoryLanguages = {
+  language_id: number;
+  language_name: string;
+};
+
+export type CallHistoryData = {
+  total_rows: 3;
+  page_size: 10;
+  offset: 0;
+};
+
+export type CallHistoryDataDetails = {
+  aggregate: CallHistoryData;
+  call_logs: CallHistoryDetails[];
+};
+
+export interface CallHistoryPageDetailsRes extends GenericRes {
+  data: CallHistoryDataDetails;
+}
+
+export type callHistoryParams = {
+  limit: number;
+  offset: number;
+};
--- src/views/protectedViews/CallHistory/index.tsx
+++ src/views/protectedViews/CallHistory/index.tsx
@@ -2,7 +2,7 @@
 import { Divider, useMediaQuery } from '@mui/material';
 import Box from '@mui/material/Box';
 import UIThemeShadowButton from 'components/UIComponents/UIStyledShadowButton';
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
   CallAgainBox,
   CallHistoryBoxContainer,
@@ -32,9 +32,86 @@ import UINewTypography from 'components/UIComponents/UINewTypography';
 import theme from 'themes/theme';
 import { FormattedMessage } from 'react-intl';
 import { UITheme2Pagination } from 'components/UIComponents/PaginationV2/Pagination.styled';
+import { CallHistoryPageDetailsRes } from 'services/callHistory/types';
+import { getUserDataClient } from 'utils/getSessionData';
+import { TokenIdType } from 'views/protectedModelViews/verification';
+import { CallHistoryService } from 'services/callHistory/callHistory.services';
+import { toast } from 'react-toastify';
+import { ErrorMessage } from 'constants/common.constants';
+import moment from 'moment';
+import { BillingPaginationBox } from '../BillingHistory/BillingHistory.styled';
+import PaginationInWords from 'components/UIComponents/PaginationINWords';
+
+export type CallHistoryPaginationType = {
+  page: number;
+  offset: number;
+  limit: number;
+};
 
 const CallHistory = () => {
   const isSmDown = useMediaQuery(theme.breakpoints.down(330));
+  const [token, setToken] = useState<TokenIdType>({ id: 0, token: '' });
+  const [callHistoryData, setCallHistoryData] = useState<CallHistoryPageDetailsRes>();
+  const [total_rows, setTotalRows] = useState(0);
+  const [filters, setFilters] = useState<CallHistoryPaginationType>({
+    page: 1,
+    limit: 20,
+    offset: 0
+  });
+
+  useEffect(() => {
+    const userToken = async () => {
+      const data = await getUserDataClient();
+      setToken({ id: data.id, token: data.token });
+    };
+
+    userToken();
+  }, []);
+
+  useEffect(() => {
+    const fetchCallHistoryDetails = async () => {
+      try {
+        if (token.token) {
+          const data = await CallHistoryService.getCallHistoryDetails(token.token);
+          if (data) {
+            setCallHistoryData(data);
+            setTotalRows(data.data.aggregate.total_rows);
+          }
+        }
+      } catch (error) {
+        toast.error(ErrorMessage);
+      }
+    };
+
+    fetchCallHistoryDetails();
+  }, [filters.limit, filters.offset, token.token]);
+
+  const scrollToTable = () => {
+    const tableElement = document.getElementById('tableSection');
+    if (tableElement) {
+      tableElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const handleChangeFilter = useCallback((value: CallHistoryPaginationType) => {
+    setFilters(value);
+  }, []);
+
+  const handleChangePage = useCallback(
+    (event: React.ChangeEvent<unknown>, value: number) => {
+      const offset = (value - 1) * filters.limit;
+      handleChangeFilter({ ...filters, page: value, offset: offset });
+      scrollToTable();
+    },
+    [filters, handleChangeFilter]
+  );
+
+  const calculateAge = (dob: string) => {
+    const birthDate = moment(dob);
+    const today = moment();
+    return today.diff(birthDate, 'years');
+  };
+
   return (
     <MainLayoutNav variant={'worker'} enlargedFooter={true}>
       <CallHistoryBoxContainer>
@@ -44,39 +121,63 @@ const CallHistory = () => {
               <FormattedMessage id="CallsHistory" />
             </UINewTypography>
           </CallHistoryText>
-          <SecondContainer>
-            <SecondSubContainer>
-              <SecondSubTextMainContainer>
-                <SecondSubFirstBox>
-                  <SecondSubFirstPartBox>
-                    <WorkerImg src="/images/workercards/workercard-img.jpeg" />
-                    <SecondSubFirstPartSecondBox>
-                      <SecondSubFirstPartSecondBoxFirstText>
-                        <CallHistoryName variant="h6" color="white.main" whiteSpace="nowrap">
-                          Kat Winter
-                        </CallHistoryName>
-                        <SecondSubFirstPartSecondBoxSecText>
-                          <UINewTypography variant="SubtitleSmallMedium" color="text.primary" lineHeight="120%">
-                            24
+          {callHistoryData?.data?.call_logs.map((list, index) => (
+            <SecondContainer key={index}>
+              <SecondSubContainer>
+                <SecondSubTextMainContainer>
+                  <SecondSubFirstBox>
+                    <SecondSubFirstPartBox>
+                      <WorkerImg src={list.link ? list.link : ''} />
+                      <SecondSubFirstPartSecondBox>
+                        <SecondSubFirstPartSecondBoxFirstText>
+                          <CallHistoryName variant="h6" color="white.main" whiteSpace="nowrap">
+                            {list.name}
+                          </CallHistoryName>
+                          <SecondSubFirstPartSecondBoxSecText>
+                            <UINewTypography variant="SubtitleSmallMedium" color="text.primary" lineHeight="120%">
+                              {calculateAge(list.dob)}
+                            </UINewTypography>
+                            <Divider orientation="vertical" flexItem sx={{ borderColor: 'text.primary' }} />
+                            <UINewTypography variant="SubtitleSmallMedium" color="text.primary" lineHeight="120%">
+                              {list.languages &&
+                                list.languages
+                                  .filter((item) => item.language_name)
+                                  .map((item) => item.language_name)
+                                  .join(', ')}
+                            </UINewTypography>
+                          </SecondSubFirstPartSecondBoxSecText>
+                        </SecondSubFirstPartSecondBoxFirstText>
+                        <CallHistoryCreditBox>
+                          <ImgBoxContainer src="/images/workercards/dollar-img.png" />
+                          <UINewTypography variant="captionLargeBold" color="text.secondary">
+                            {list.credits_used} credits/hr
                           </UINewTypography>
-                          <Divider orientation="vertical" flexItem sx={{ borderColor: 'text.primary' }} />
-                          <UINewTypography variant="SubtitleSmallMedium" color="text.primary" lineHeight="120%">
-                            English, Spanish
-                          </UINewTypography>
-                        </SecondSubFirstPartSecondBoxSecText>
-                      </SecondSubFirstPartSecondBoxFirstText>
-                      <CallHistoryCreditBox>
-                        <ImgBoxContainer src="/images/workercards/dollar-img.png" />
-                        <UINewTypography variant="captionLargeBold" color="text.secondary">
-                          20 credits/hr
+                        </CallHistoryCreditBox>
+                      </SecondSubFirstPartSecondBox>
+                    </SecondSubFirstPartBox>
+                    {!isSmDown && (
+                      <SecondSubFirstPartThiredBox marginRight={{ sm: '32px' }}>
+                        <UINewTypography variant="buttonLargeMenu" color="text.primary">
+                          Duration: {list.duration && moment(list.duration).format('h:mm:ss a')}
                         </UINewTypography>
-                      </CallHistoryCreditBox>
-                    </SecondSubFirstPartSecondBox>
-                  </SecondSubFirstPartBox>
-                  {!isSmDown && (
-                    <SecondSubFirstPartThiredBox marginRight={{ sm: '32px' }}>
+                        <CreditUsedBox>
+                          <UINewTypography variant="buttonLargeMenu" color="text.primary" whiteSpace="nowrap">
+                            Credits used:
+                          </UINewTypography>
+                          <SecondSubFirstPartThiredBoxText>
+                            <ImgBoxContainer src="/images/workercards/dollar-img.png" />
+                            <UINewTypography variant="buttonLargeMenu" color="text.primary">
+                              {list.credits_used}
+                            </UINewTypography>
+                          </SecondSubFirstPartThiredBoxText>
+                        </CreditUsedBox>
+                      </SecondSubFirstPartThiredBox>
+                    )}
+                  </SecondSubFirstBox>
+                  {isSmDown && (
+                    <SecondSubFirstPartThiredBox gap="8px !important">
                       <UINewTypography variant="buttonLargeMenu" color="text.primary">
-                        Duration: 30 mins
+                        Duration: {list.duration && moment(list.duration).format('h:mm:ss a')}
                       </UINewTypography>
                       <CreditUsedBox>
                         <UINewTypography variant="buttonLargeMenu" color="text.primary" whiteSpace="nowrap">
@@ -85,51 +186,51 @@ const CallHistory = () => {
                         <SecondSubFirstPartThiredBoxText>
                           <ImgBoxContainer src="/images/workercards/dollar-img.png" />
                           <UINewTypography variant="buttonLargeMenu" color="text.primary">
-                            35
+                            {list.credits_used}
                           </UINewTypography>
                         </SecondSubFirstPartThiredBoxText>
                       </CreditUsedBox>
                     </SecondSubFirstPartThiredBox>
                   )}
-                </SecondSubFirstBox>
-                {isSmDown && (
-                  <SecondSubFirstPartThiredBox gap="8px !important">
-                    <UINewTypography variant="buttonLargeMenu" color="text.primary">
-                      Duration: 30 mins
-                    </UINewTypography>
-                    <CreditUsedBox>
-                      <UINewTypography variant="buttonLargeMenu" color="text.primary" whiteSpace="nowrap">
-                        Credits used:
-                      </UINewTypography>
-                      <SecondSubFirstPartThiredBoxText>
-                        <ImgBoxContainer src="/images/workercards/dollar-img.png" />
-                        <UINewTypography variant="buttonLargeMenu" color="text.primary">
-                          35
-                        </UINewTypography>
-                      </SecondSubFirstPartThiredBoxText>
-                    </CreditUsedBox>
-                  </SecondSubFirstPartThiredBox>
-                )}
-                <CallAgainBox>
-                  <UIThemeShadowButton variant="contained" sx={{ width: '100%', maxWidth: { xs: '363px', sm: '173px' } }}>
-                    <Box sx={{ display: 'flex', gap: 1.25 }}>
-                      <SecImgBoxContainer src="/images/home-connect-instantly-img.png" />
-                      <Box sx={{ whiteSpace: 'nowrap' }}>
-                        <UINewTypography variant="bodySemiBold" color="white.main">
-                          <FormattedMessage id="CallAgain" />
-                        </UINewTypography>
+                  <CallAgainBox>
+                    <UIThemeShadowButton variant="contained" sx={{ width: '100%', maxWidth: { xs: '363px', sm: '173px' } }}>
+                      <Box sx={{ display: 'flex', gap: 1.25 }}>
+                        <SecImgBoxContainer src="/images/home-connect-instantly-img.png" />
+                        <Box sx={{ whiteSpace: 'nowrap' }}>
+                          <UINewTypography variant="bodySemiBold" color="white.main">
+                            <FormattedMessage id="CallAgain" />
+                          </UINewTypography>
+                        </Box>
                       </Box>
-                    </Box>
-                  </UIThemeShadowButton>
-                </CallAgainBox>
-              </SecondSubTextMainContainer>
-            </SecondSubContainer>
-            <DividerContainer orientation="horizontal" flexItem />
-          </SecondContainer>
+                    </UIThemeShadowButton>
+                  </CallAgainBox>
+                </SecondSubTextMainContainer>
+              </SecondSubContainer>
+
+              <DividerContainer orientation="horizontal" flexItem />
+            </SecondContainer>
+          ))}
         </CallHistoryMainContainer>
-        <CallHistoryPaginationContainer>
-          <UITheme2Pagination />
-        </CallHistoryPaginationContainer>
+        {total_rows > 0 && (
+          <CallHistoryPaginationContainer>
+            {total_rows > 0 && (
+              <BillingPaginationBox>
+                <UITheme2Pagination
+                  page={filters.page}
+                  count={total_rows ? Math.ceil(total_rows / filters.limit) : 1}
+                  onChange={handleChangePage}
+                />
+                <PaginationInWords
+                  page={filters.page}
+                  limit={filters.limit}
+                  total_rows={total_rows}
+                  offset={filters.offset}
+                  isEscort={true}
+                />
+              </BillingPaginationBox>
+            )}
+          </CallHistoryPaginationContainer>
+        )}
       </CallHistoryBoxContainer>
     </MainLayoutNav>
   );
