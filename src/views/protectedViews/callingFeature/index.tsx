930b3271b788390820ad0214635776b2472e3de4
Muskan Lalani <mukan.divtech@gmail.com>
2024-07-05 10:39:34 +0530

    ui customization of ui ongoing screen

--- src/views/protectedViews/callingFeature/index.tsx
+++ src/views/protectedViews/callingFeature/index.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { CometChatIncomingCall, CometChatOngoingCall, CometChatOutgoingCall } from '@cometchat/chat-uikit-react';
 import { useCallFeatureContext } from '../../../../context/CallFeatureContext';
+import RingingModel from '../videoCalling/RingingModel';
 
 const CallFeature = () => {
   const { call, handleCancelCall, isCallAccepted, isCustomer } = useCallFeatureContext();
@@ -8,7 +9,7 @@ const CallFeature = () => {
   return (
     <>
       {!isCustomer && <CometChatIncomingCall call={call} />}
-      {call && !isCallAccepted && <CometChatOutgoingCall call={call} onCloseClicked={handleCancelCall} />}
+      {call && !isCallAccepted && <CometChatOutgoingCall call={call} customView={<RingingModel onClose={handleCancelCall} />} />}
       {call && isCallAccepted && <CometChatOngoingCall sessionID={call?.getSessionId()} />}
     </>
   );
