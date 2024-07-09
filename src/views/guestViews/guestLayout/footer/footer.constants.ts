4cb67a615bac6195240132f72a0fe6c62061d116
dhruv-divtech <dhruv.divtech@gmail.com>
2024-07-09 11:09:32 +0530

    privacy statement page done

--- src/views/guestViews/guestLayout/footer/footer.constants.ts
+++ src/views/guestViews/guestLayout/footer/footer.constants.ts
@@ -1,5 +1,5 @@
 export const FooterCityList = [
-  { name: 'PrivacyStatement', link: '/' },
+  { name: 'PrivacyStatement', link: '/privacy-statement' },
   { name: 'CookieStatement', link: '/' },
   { name: 'TermsConditions', link: '/terms-and-condition' }
 ];
---
+++ src/views/guestViews/privacyPolicyPage/privacyPolicy.styled.tsx
@@ -0,0 +1,20 @@
+import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
+import Typography from '@mui/material/Typography';
+
+export const PrivacyPolicyMainContainer = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: theme.spacing(7)
+}));
+
+export const FirstTextContainer = styled(Typography)(({ theme }) => ({
+  fontSize: '16px',
+  fontWeight: 600,
+  lineHeight: '25.6px'
+}));
+export const FirstBoxMainContainer = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  gap: theme.spacing(5)
+}));
