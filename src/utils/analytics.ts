ba8529b07ad1435dd3dcd6383f966bf7cb38e19d
Pramod Mahato <pramodkrmahato@hotmail.com>
2024-08-11 16:23:37 +0530

    added isJON

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,29 +1,21 @@
 export const gaEventTrigger = (action: string, data: any) => {
   if (window.gtag) {
     try {
-      const parsedValue = tryParseJSON(data.value);
-      if (parsedValue) {
-        const flattenedValue = flattenObject(parsedValue, 'custom_');
-        data = { ...data, ...flattenedValue };
+      // Check if the value is a string but not a JSON object
+      const isPlainString = typeof data.value === 'string' && !isJSON(data.value);
+
+      if (!isPlainString) {
+        const parsedValue = tryParseJSON(data.value);
+        if (parsedValue) {
+          const flattenedValue = flattenObject(parsedValue, 'custom_');
+          data = { ...data, ...flattenedValue };
+        }
       }
     } catch (e) {
       console.error('Error processing value field:', e);
     }
     window.gtag('event', action, data);
   }
-};
-
-// Helper function to flatten a nested object with a custom prefix and replace '.' with '_'
-const flattenObject = (obj: any, prefix: string = '', parentKey: string = ''): any => {
-  return Object.keys(obj).reduce((acc:any, key) => {
-    const newKey = `${prefix}${parentKey ? `${parentKey}_` : ''}${key}`.replace(/\./g, '_');
-    if (typeof obj[key] === 'object' && obj[key] !== null) {
-      Object.assign(acc, flattenObject(obj[key], prefix, newKey));
-    } else {
-      acc[newKey] = obj[key];
-    }
-    return acc;
-  }, {});
 };
 
 // Helper function to safely parse JSON
@@ -37,4 +29,27 @@ const tryParseJSON = (jsonString: string): any => {
     console.error('Invalid JSON string:', jsonString);
   }
   return null;
+};
+
+// Helper function to check if a string is valid JSON
+const isJSON = (str: string): boolean => {
+  try {
+    JSON.parse(str);
+    return true;
+  } catch (e) {
+    return false;
+  }
+};
+
+// Helper function to flatten a nested object
+const flattenObject = (obj: any, prefix: string = '', parentKey: string = ''): any => {
+  return Object.keys(obj).reduce((acc:any, key) => {
+    const newKey = `${prefix}${parentKey ? `${parentKey}_` : ''}${key}`.replace(/\./g, '_');
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      Object.assign(acc, flattenObject(obj[key], prefix, newKey));
+    } else {
+      acc[newKey] = obj[key];
+    }
+    return acc;
+  }, {});
 };
