bd01feced7243ed571a7ef5ab5d8947ed6d9c93d
Pramod Mahato <pramodkrmahato@hotmail.com>
2024-08-11 16:12:41 +0530

    tryParse

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,19 +1,21 @@
 export const gaEventTrigger = (action: string, data: any) => {
   if (window.gtag) {
     try {
-      const parsedValue = JSON.parse(data.value);
-      const flattenedValue = flattenObject(parsedValue, 'custom_');
-      data = { ...data, ...flattenedValue };
-      window.gtag('event', action, data);
+      const parsedValue = tryParseJSON(data.value);
+      if (parsedValue) {
+        const flattenedValue = flattenObject(parsedValue, 'custom_');
+        data = { ...data, ...flattenedValue };
+      }
     } catch (e) {
-      console.error('Error parsing value field:', e);
+      console.error('Error processing value field:', e);
     }
+    window.gtag('event', action, data);
   }
 };
 
-//Helper function to flatten a nested object
+// Helper function to flatten a nested object with a custom prefix and replace '.' with '_'
 const flattenObject = (obj: any, prefix: string = '', parentKey: string = ''): any => {
-  return Object.keys(obj).reduce((acc :any, key) => {
+  return Object.keys(obj).reduce((acc:any, key) => {
     const newKey = `${prefix}${parentKey ? `${parentKey}_` : ''}${key}`.replace(/\./g, '_');
     if (typeof obj[key] === 'object' && obj[key] !== null) {
       Object.assign(acc, flattenObject(obj[key], prefix, newKey));
@@ -22,4 +24,17 @@ const flattenObject = (obj: any, prefix: string = '', parentKey: string = ''): a
     }
     return acc;
   }, {});
+};
+
+// Helper function to safely parse JSON
+const tryParseJSON = (jsonString: string): any => {
+  try {
+    const obj = JSON.parse(jsonString);
+    if (obj && typeof obj === "object") {
+      return obj;
+    }
+  } catch (e) {
+    console.error('Invalid JSON string:', jsonString);
+  }
+  return null;
 };
