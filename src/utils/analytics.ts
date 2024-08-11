baa9afcdaa460be30332745b8b57705483040ceb
Pramod Mahato <pramodkrmahato@hotmail.com>
2024-08-11 15:58:37 +0530

    analytics value obj flattened

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,5 +1,25 @@
 export const gaEventTrigger = (action: string, data: any) => {
   if (window.gtag) {
-    window.gtag('event', action, data);
+    try {
+      const parsedValue = JSON.parse(data.value);
+      const flattenedValue = flattenObject(parsedValue, 'custom_');
+      data = { ...data, ...flattenedValue };
+      window.gtag('event', action, data);
+    } catch (e) {
+      console.error('Error parsing value field:', e);
+    }
   }
+};
+
+//Helper function to flatten a nested object
+const flattenObject = (obj: any, prefix: string = '', parentKey: string = ''): any => {
+  return Object.keys(obj).reduce((acc :any, key) => {
+    const newKey = `${prefix}${parentKey ? `${parentKey}_` : ''}${key}`.replace(/\./g, '_');
+    if (typeof obj[key] === 'object' && obj[key] !== null) {
+      Object.assign(acc, flattenObject(obj[key], prefix, newKey));
+    } else {
+      acc[newKey] = obj[key];
+    }
+    return acc;
+  }, {});
 };
