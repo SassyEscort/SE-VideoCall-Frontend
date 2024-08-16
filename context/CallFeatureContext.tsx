3b148fcd108e83757f6759accb651fd5ea471231
dhruv-divtech <dhruv.divtech@gmail.com>
2024-08-16 17:37:13 +0530

    credit amount changes

--- context/CallFeatureContext.tsx
+++ context/CallFeatureContext.tsx
@@ -453,7 +453,7 @@ export const CallFeatureProvider = ({ children }: { children: ReactNode }) => {
 
   useEffect(() => {
     const credit = searchParams.get('credit');
-    const totalBal = searchParams.get('total_credits_after_txn');
+    const totalBal = searchParams.get('total_amount_after_txn');
     setBalance(Number(totalBal));
     setAddedCredits(Number(credit));
     if (credit) {
