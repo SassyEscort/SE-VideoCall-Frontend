a42d7376e211e80d7603ce0ace51399694d68d09
dhruv-divtech <dhruv.divtech@gmail.com>
2024-08-20 15:51:16 +0530

    sitemap changes

--- src/app/(modelWithoutAuth)/sitemap/sitemap.ts
+++ src/app/(modelWithoutAuth)/sitemap/sitemap.ts
@@ -3,9 +3,14 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://flirtbate.com/sitemap/sitemap.xml',
+      url: 'https://flirtbate.com/',
       lastModified: new Date(),
       priority: 1
+    },
+    {
+      url: 'https://flirtbate.com/model/sitemap.xml',
+      lastModified: new Date(),
+      priority: 0.8
     }
   ];
 }
