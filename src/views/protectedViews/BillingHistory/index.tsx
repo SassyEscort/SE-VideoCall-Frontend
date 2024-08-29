f86c43506cfa869293c6f3d67a43f95ee65300e8
muskanlalani123 <mukan.divtech@gmail.com>
2024-08-29 11:05:10 +0530

    removed $ in billing history

--- src/views/protectedViews/BillingHistory/index.tsx
+++ src/views/protectedViews/BillingHistory/index.tsx
@@ -158,7 +158,7 @@ const BillingHistory = () => {
                               <FormattedMessage id="ViewDetails" />
                             </TextBoxContainer>
                           ) : (
-                            `$ ${list?.free_credits ? list?.free_credits : list?.amount}`
+                            `${list?.free_credits ? list?.free_credits : `$${list?.amount}`}`
                           )}
                         </DollarBillingValue>
                       </Box>
