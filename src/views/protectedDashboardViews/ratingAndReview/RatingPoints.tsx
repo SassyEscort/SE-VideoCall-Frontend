b8d72bac38d4ff8fc20b48ec497905e0c82ad809
Muskan Lalani <mukan.divtech@gmail.com>
2024-09-09 14:25:53 +0530

    ui changes

--- src/views/protectedDashboardViews/ratingAndReview/RatingPoints.tsx
+++ src/views/protectedDashboardViews/ratingAndReview/RatingPoints.tsx
@@ -49,7 +49,11 @@ const RatingPoints = ({
               <UINewTypography variant="bodyLight">{item.id}</UINewTypography>
               <StarRoundedIcon sx={{ color: '#FFB400', width: '20px', height: '20px' }} />
             </TextAndStarBoxContainer>
-            <BorderLinearProgress variant="determinate" value={ratingAndReview?.[item.label as keyof RatingAndReviewDetailsInfo] || 0} />
+            <BorderLinearProgress
+              isShowPercentage={isShowPercentage}
+              variant="determinate"
+              value={ratingAndReview?.[item.label as keyof RatingAndReviewDetailsInfo] || 0}
+            />
             {isShowPercentage && (
               <UINewTypography variant="bodyLight">
                 {printPercentageRating(ratingAndReview?.[item.label as keyof RatingAndReviewDetailsInfo] || 0)}%
