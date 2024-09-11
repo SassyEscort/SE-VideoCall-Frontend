79809eee35b91d9305c3c64778ab5bb85fc2625e
dhruv-divtech <dhruv.divtech@gmail.com>
2024-09-11 16:13:14 +0530

    canonical set for home page and details page

--- src/app/(guest)/page.tsx
+++ src/app/(guest)/page.tsx
@@ -1,5 +1,13 @@
+import { Metadata } from 'next';
 import HomeContainer from 'views/guestViews/homePage';
 
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    alternates: {
+      canonical: `https://flirtbate.com`
+    }
+  };
+}
 const Home = () => {
   return <HomeContainer />;
 };
