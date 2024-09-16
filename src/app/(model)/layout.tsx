462975a80feb38bf9e92208604cd12efa7d4d4d5
dhruv-divtech <dhruv.divtech@gmail.com>
2024-09-16 18:01:04 +0530

    guard changes for customer

--- src/app/(model)/layout.tsx
+++ src/app/(model)/layout.tsx
@@ -1,6 +1,7 @@
 import Box from '@mui/material/Box';
 import { PROVIDERCUSTOM_TYPE } from 'constants/signUpConstants';
 import { getLoggedInUser } from 'utils/getSessionData';
+import RedirectGuardCustomer from 'utils/route-guard/RedirectGuardCustomer';
 import ModelFooter from 'views/modelViews/modelLayout/footer';
 import HeaderModelComponent from 'views/modelViews/modelLayout/Header';
 import Header from 'views/protectedViews/protectedLayout/Header';
@@ -20,12 +21,14 @@ export default async function Layout({ children }: { children: React.ReactNode }
   const authUser: AuthUser | null = await getLoggedInUser();
 
   return (
-    <Box>
-      {authUser?.user?.provider === PROVIDERCUSTOM_TYPE.PROVIDERCUSTOM ? <Header variant="dashboard" /> : <HeaderModelComponent />}
-      <main>
-        <Box sx={{ mt: 10 }}>{children}</Box>
-      </main>
-      <ModelFooter />
-    </Box>
+    <RedirectGuardCustomer>
+      <Box>
+        {authUser?.user?.provider === PROVIDERCUSTOM_TYPE.PROVIDERCUSTOM ? <Header variant="dashboard" /> : <HeaderModelComponent />}
+        <main>
+          <Box sx={{ mt: 10 }}>{children}</Box>
+        </main>
+        <ModelFooter />
+      </Box>
+    </RedirectGuardCustomer>
   );
 }
