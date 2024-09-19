4d9a3ca95df757cae150cf51dc13c5c889d4f882
dhruv-divtech <dhruv.divtech@gmail.com>
2024-09-19 17:16:37 +0530

    add title in details page

--- src/app/(guest)/details/[id]/page.tsx
+++ src/app/(guest)/details/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next';
+import Head from 'next/head';
 import EscortDetailPage from 'views/guestViews/details/EscortDetailPage';
 
 export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
@@ -10,8 +11,16 @@ export async function generateMetadata({ params }: { params: { id: string } }): 
   };
 }
 
-const WorkerDetailPage = () => {
-  return <>{<EscortDetailPage />}</>;
+const WorkerDetailPage = ({ params }: { params: { id: string } }) => {
+  const modelId = params.id;
+  return (
+    <>
+      <Head>
+        <link href={`https://flirtbate.com/details/${modelId}`} />
+      </Head>
+      {<EscortDetailPage />}
+    </>
+  );
 };
 
 export default WorkerDetailPage;
