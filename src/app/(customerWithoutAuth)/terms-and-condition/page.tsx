363792d569ed98b566a1f1542617d269f76e7a10
dhruv-divtech <dhruv.divtech@gmail.com>
2024-09-20 15:39:30 +0530

    file changes.

--- src/app/(customerWithoutAuth)/terms-and-condition/page.tsx
+++ src/app/(customerWithoutAuth)/terms-and-condition/page.tsx
@@ -1,15 +1,8 @@
-import Head from 'next/head';
 import TermaAndCondition from 'views/guestViews/termsconditionPage';
 
 const page = () => {
   return (
     <>
-      <Head>
-        <title>Page Title</title>
-        <meta name="description" content="Page description" />
-        <meta property="og:title" content="Open Graph Title" />
-        <meta property="og:description" content="Open Graph description" />
-      </Head>
       <TermaAndCondition />
     </>
   );
