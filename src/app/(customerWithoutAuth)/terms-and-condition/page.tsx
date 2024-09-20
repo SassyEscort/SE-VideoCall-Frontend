c239fc421a5e95b8e3f30be4c235686bad502d50
dhruv-divtech <dhruv.divtech@gmail.com>
2024-09-20 16:22:27 +0530

    body tag changes

--- src/app/(customerWithoutAuth)/terms-and-condition/page.tsx
+++ src/app/(customerWithoutAuth)/terms-and-condition/page.tsx
@@ -3,6 +3,12 @@ import TermaAndCondition from 'views/guestViews/termsconditionPage';
 const page = () => {
   return (
     <>
+      <body>
+        <title>Page Title</title>
+        <meta name="description" content="Page description" />
+        <meta property="og:title" content="Open Graph Title" />
+        <meta property="og:description" content="Open Graph description" />
+      </body>
       <TermaAndCondition />
     </>
   );
