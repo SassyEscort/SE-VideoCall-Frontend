46f9a0f02c4b7738969f57471129513f37c257a9
dhruv-divtech <dhruv.divtech@gmail.com>
2024-10-03 18:56:12 +0530

    set on load tawk context

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
+import React from 'react';
 import type { Metadata } from 'next';
 import ProviderWrapper from './ProviderWrapper';
 import { SEO_DATA } from 'constants/seoConstants';
 import { AuthFeaturProvider } from '../../context/AuthContext';
+import { TawkProvider } from '../../context/TawkContext';
 
 export const metadata: Metadata = {
   title: SEO_DATA.TITLE,
@@ -117,22 +119,39 @@ export default function RootLayout({
             __html: `
             var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
             (function(){
-              var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-              s1.async=true;
-              s1.src='https://embed.tawk.to/66f6b30fe5982d6c7bb5a2bd/1i8pqmtl1';
-              s1.charset='UTF-8';
-              s1.setAttribute('crossorigin','*');
-              s0.parentNode.insertBefore(s1,s0);
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/66fbef6be5982d6c7bb726ab/1i941ug0n';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
             })();
-          `
+            `
           }}
         />
+        {/* <script
+          id="tawk-to"
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/66f6b3774cbc4814f7dff7db/1i8pqq3gj';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+            `
+          }}
+        /> */}
       </head>
       <body>
         <ProviderWrapper>
-          <>
-            <AuthFeaturProvider>{children}</AuthFeaturProvider>
-          </>
+          <AuthFeaturProvider>
+            <TawkProvider>{children}</TawkProvider>
+          </AuthFeaturProvider>
         </ProviderWrapper>
       </body>
     </html>
