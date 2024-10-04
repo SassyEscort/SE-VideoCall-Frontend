0baea96f6c87dfb691d93606bc26159fc2e8b066
Mayur Bhagat <mayur.divtech@gmail.com>
2024-10-04 15:56:07 +0530

    chat api servive

---
+++ src/services/chatServices/chat.service.ts
@@ -0,0 +1,38 @@
+import axios, { AxiosError } from 'axios';
+
+export interface IChatMessageParams {
+  senderUID: string;
+  receiverUID: string;
+  message: string;
+}
+
+export interface IMessageResponse {
+  data: {
+    id: string;
+    senderUID: string;
+    message: string;
+    receiverUID: string;
+    createdAt: {
+      _seconds: number;
+      _nanoseconds: number;
+    };
+  };
+}
+
+export class CallingService {
+  static sendChatMessage = async (params: IChatMessageParams, token: string): Promise<IMessageResponse> => {
+    try {
+      const res = await axios.post(process.env.NEXT_PUBLIC_API_BASE_URL + `/v1/chat/send-message`, params, {
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: token
+        }
+      });
+
+      return res.data.data;
+    } catch (err: any) {
+      const error: AxiosError = err;
+      return error.response?.data as IMessageResponse;
+    }
+  };
+}
--- src/views/guestViews/chat/index.tsx
+++ src/views/guestViews/chat/index.tsx
@@ -6,9 +6,23 @@ import { Message } from 'yup';
 import ChatSidbar from './ChatSidbar';
 import { ChatMainBoxContainer } from './Chat.styled';
 import ChatDescription from './ChatDescription';
+import { usePathname } from 'next/navigation';
+import { useSession } from 'next-auth/react';
+import { User } from 'app/(guest)/layout';
+// import { ErrorMessage } from 'constants/common.constants';
+// import { toast } from 'react-toastify';
+// import { getUserDataClient } from 'utils/getSessionData';
+// import { TokenIdType } from 'views/protectedModelViews/verification';
 
 const ChatFeature = () => {
   const [messages, setMessages] = useState<Message[]>([]);
+  // const [token, setToken] = useState<TokenIdType>({ id: 0, token: '' });
+
+  const authSession = useSession();
+  const customerUser = authSession?.data?.user as User;
+
+  const path = usePathname();
+  const modelUserName = path?.split('/')?.[2];
 
   useEffect(() => {
     // Reference to the messages collection
@@ -17,8 +31,11 @@ const ChatFeature = () => {
 
     // Real-time listener for Firestore collection
     const unsubscribe = onSnapshot(q, (snapshot) => {
+      console.log('snapshotcall');
       const messagesData: any[] = [];
       snapshot.forEach((doc) => {
+        console.log('doc data', doc.data());
+
         messagesData.push({ id: doc.id, ...doc.data() });
       });
       setMessages(messagesData);
@@ -27,7 +44,34 @@ const ChatFeature = () => {
     // Clean up the listener on unmount
     return () => unsubscribe();
   }, []);
-  console.log('messages', messages);
+  console.log('messages', messages, customerUser, modelUserName);
+
+  // useEffect(() => {
+  //   const userToken = async () => {
+  //     const data = await getUserDataClient();
+  //     setToken({ id: data.id, token: data.token });
+  //   };
+
+  //   userToken();
+  // }, []);
+
+  // useEffect(() => {
+  //   const fetchEarningHistoryDetails = async () => {
+  //     try {
+  //       if (token.token) {
+  //         setIsLoading(true);
+  //         const data = await ModelBillingHistoryService.getBillingHistoryDetails(params, token.token);
+  //         if (data) {
+  //           setIsLoading(false);
+  //         }
+  //       }
+  //     } catch (error) {
+  //       toast.error(ErrorMessage);
+  //     }
+  //   };
+
+  //   fetchEarningHistoryDetails();
+  // }, []);
 
   return (
     <ChatMainBoxContainer>
