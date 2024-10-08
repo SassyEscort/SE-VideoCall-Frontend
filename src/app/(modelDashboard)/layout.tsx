a9810b49859efa0ff09c793535a5e037223690cc
muskanlalani123 <mukan.divtech@gmail.com>
2024-10-08 14:51:34 +0530

    changed model last active api

--- src/app/(modelDashboard)/layout.tsx
+++ src/app/(modelDashboard)/layout.tsx
@@ -1,11 +1,13 @@
 import Box from '@mui/material/Box';
 import ModelGuard from 'utils/route-guard/ModelGuard';
 import ModelFooter from 'views/modelViews/modelLayout/footer';
+import ModelLastActive from 'views/protectedModelViews/ModelLastAvtive';
 import Header from 'views/protectedViews/protectedLayout/Header';
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   return (
     <ModelGuard>
+      <ModelLastActive />
       <Box>
         <Header variant="dashboard" />
         <main>
