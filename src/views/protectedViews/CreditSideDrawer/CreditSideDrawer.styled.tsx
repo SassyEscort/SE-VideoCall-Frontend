68fa1642b5a101112d6ba34b5db496d083dc34ba
dhruv-divtech <dhruv.divtech@gmail.com>
2024-10-14 19:00:20 +0530

    side drawer cahnges

--- src/views/protectedViews/CreditSideDrawer/CreditSideDrawer.styled.tsx
+++ src/views/protectedViews/CreditSideDrawer/CreditSideDrawer.styled.tsx
@@ -1,16 +1,39 @@
 import { Box, Drawer, styled } from '@mui/material';
 import UINewTypography from 'components/UIComponents/UINewTypography';
 
+export const CreditLoaderBox = styled(Box)(() => ({
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  height: '100%'
+}));
+
+export const CreditPopularChip = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  gap: theme.spacing(0.75),
+  position: 'absolute',
+  alignItems: 'center',
+  top: '-25%',
+  background: theme.palette.text.secondary,
+  boxShadow: '0px -2px 20px 0px #FD85CAA3',
+  padding: '6px 12px',
+  borderRadius: '4px'
+}));
+
 export const CreditSideMainDrawer = styled(Drawer)(({ theme }) => ({
   '& .MuiDrawer-paper': {
     width: '100%',
-    maxWidth: 400,
+    height: '100%',
+    maxWidth: '400px',
     backgroundColor: '#1E0815',
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
     borderRight: 'none',
-    scrollbarWidth: 'none'
+    scrollbarWidth: 'none',
+    [theme.breakpoints.down('sm')]: {
+      maxWidth: '393px'
+    }
   }
 }));
 
@@ -21,16 +44,18 @@ export const CreditsHeader = styled(Box)(({ theme }) => ({
   paddingTop: theme.spacing(3),
   alignItems: 'center',
   gap: theme.spacing(1.5),
-  width: 352
+  width: '100%',
+  maxWidth: '352px'
 }));
 
 export const CreditsContent = styled(Box)(({ theme }) => ({
   width: '100%',
+  height: '100%',
   display: 'flex',
   flexDirection: 'column',
-  alignItems: 'start',
-  gap: theme.spacing(5),
-  overflowY: 'auto'
+  justifyContent: 'start',
+  gap: theme.spacing(4),
+  position: 'relative'
 }));
 
 export const TitleSerachBox = styled(Box)(({ theme }) => ({
@@ -42,15 +67,16 @@ export const TitleSerachBox = styled(Box)(({ theme }) => ({
 export const CurrentBalanceBox = styled(Box)(({ theme }) => ({
   backgroundColor: 'Transparent',
   zIndex: '1',
-  height: '154px',
-  width: '100%'
-}));
-
-export const CurrentBalanceInnerBox = styled(Box)(({ theme }) => ({
+  height: '100%',
+  maxHeight: '154px',
+  width: '100%',
   display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'Center',
-  gap: theme.spacing(1.5)
+  alignItems: 'start',
+  gap: theme.spacing(1.5),
+  paddingTop: theme.spacing(3.25),
+  paddingBottom: theme.spacing(3.25),
+  paddingRight: theme.spacing(3),
+  paddingLeft: theme.spacing(3)
 }));
 
 export const CurrentBalanceTypography = styled(UINewTypography)(({ theme }) => ({
@@ -67,7 +93,7 @@ export const MainImageBox = styled(Box)(({ theme }) => ({
   backgroundImage: `
   linear-gradient(
     180deg, 
-    rgba(0, 0, 0, 0.7) 0%, 
+    rgba(0, 0, 0, 0.9) 0%, 
     rgba(0, 0, 0, 0) 100%   
   ),
   url(/images/credits/CreditsMainimg.png)
@@ -79,7 +105,50 @@ export const MainImageBox = styled(Box)(({ theme }) => ({
 
 export const CreditListMainBox = styled(Box)(({ theme }) => ({
   width: '100%',
+  height: '100%',
   backgroundColor: '#07030E',
   zIndex: '1',
-  height: '100%'
+  display: 'flex',
+  flexDirection: 'column',
+  gap: theme.spacing(4),
+  paddingTop: theme.spacing(2.5),
+  paddingLeft: theme.spacing(1.875),
+  paddingRight: theme.spacing(2.75),
+  borderTopLeftRadius: '24px',
+  borderTopRightRadius: '24px',
+  borderBottomLeftRadius: '0',
+  borderBottomRightRadius: '0',
+  position: 'relative',
+  overflowY: 'scroll',
+  scrollbarWidth: 'none',
+  maxHeight: '73vh',
+  paddingBottom: theme.spacing(2)
+}));
+
+export const CreditTypography = styled(UINewTypography)(({ theme }) => ({
+  fontSize: '24px',
+  fontWeight: '600',
+  lineHeight: '28.8px'
+}));
+
+export const CreditListContainer = styled(Box)(() => ({
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  padding: '20px 16px',
+  borderRadius: '8px',
+  cursor: 'pointer',
+  border: '1px solid #D4D3D63D'
+}));
+
+export const CreditInfoBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  gap: theme.spacing(0.5)
+}));
+
+export const CreditPriceBox = styled(Box)(({ theme }) => ({
+  display: 'flex',
+  alignItems: 'center',
+  gap: theme.spacing(2)
 }));
--- src/views/protectedViews/CreditSideDrawer/index.tsx
+++ src/views/protectedViews/CreditSideDrawer/index.tsx
@@ -1,48 +1,292 @@
-import { memo } from 'react';
+import { memo, useCallback, useEffect, useState } from 'react';
 
 import {
+  CreditInfoBox,
+  CreditListContainer,
   CreditListMainBox,
+  CreditLoaderBox,
+  CreditPopularChip,
+  CreditPriceBox,
   CreditSideMainDrawer,
+  CreditTypography,
   CreditsContent,
   CreditsHeader,
   CurrentBalanceBox,
-  CurrentBalanceInnerBox,
   CurrentBalanceTypography,
   MainImageBox,
   TitleSerachBox
 } from './CreditSideDrawer.styled';
-import { Box, IconButton } from '@mui/material';
+import { Box, CircularProgress, IconButton } from '@mui/material';
 import { Close } from '@mui/icons-material';
 import UINewTypography from 'components/UIComponents/UINewTypography';
+import { CustomerCredit, ModelCreditRes } from 'services/customerCredit/customerCredit.service';
+import { getUserDataClient } from 'utils/getSessionData';
+import { TokenIdType } from 'views/protectedModelViews/verification';
+import { ModelDetailsService } from 'services/modelDetails/modelDetails.services';
+import { useCallFeatureContext } from '../../../../context/CallFeatureContext';
+import { useRouter } from 'next/navigation';
+import { gaEventTrigger } from 'utils/analytics';
+import { CustomerDetails, CustomerDetailsService } from 'services/customerDetails/customerDetails.services';
+import { FormattedMessage } from 'react-intl';
 
 const CreditSideDrawer = ({ open, handleClose }: { open: boolean; handleClose: () => void }) => {
+  const [creditsListing, setCreditsListing] = useState<ModelCreditRes[]>([]);
+  const [token, setToken] = useState<TokenIdType>({ id: 0, token: '' });
+  const [balance, setBalance] = useState(0);
+  const [isLoading, setIsLoading] = useState(false);
+  const [customerDetails, setCustomerDetails] = useState<CustomerDetails>();
+
+  const { user } = useCallFeatureContext();
+  const customerData = JSON.parse(user || '{}');
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const userToken = async () => {
+      const data = await getUserDataClient();
+      if (data) {
+        setToken({ id: data.id, token: data.token });
+      }
+    };
+    userToken();
+  }, []);
+
+  const getCreditsListing = useCallback(async () => {
+    if (token.token) {
+      setIsLoading(true);
+      const getModel = await CustomerCredit.getCustomerCredit(token.token);
+      setCreditsListing(getModel.data);
+      setIsLoading(false);
+    }
+  }, [token.token]);
+
+  const getCustomerCredit = useCallback(async () => {
+    if (token.token) {
+      const getModel = await ModelDetailsService.getModelWithDraw(token.token);
+      setBalance(getModel?.data?.credits);
+    }
+  }, [token.token]);
+
+  const handleCreditClick = async (listCredit: ModelCreditRes) => {
+    setIsLoading(true);
+    const customerInfo = {
+      email: customerData?.customer_email,
+      name: customerData?.customer_name,
+      username: customerData?.customer_user_name,
+      model_username: '',
+      plan_details: listCredit,
+      source: 'Credit Page'
+    };
+
+    gaEventTrigger('Credits_Purchase_Initiated', {
+      action: 'Credits_Purchase_Initiated',
+      category: 'Button',
+      label: 'Credits_Purchase_Initiated',
+      value: JSON.stringify(customerInfo)
+    });
+    const res = await CustomerCredit.modelCreditAmount(token.token, listCredit.id, 0, false);
+    if (res) {
+      router.push(res?.data?.url);
+    }
+    setIsLoading(false);
+  };
+
+  const FetchCustomerDetails = async () => {
+    setIsLoading(true);
+
+    try {
+      const customerData = await CustomerDetailsService.customerModelDetails(token.token);
+      setCustomerDetails(customerData?.data);
+    } catch (error) {}
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getCreditsListing();
+    getCustomerCredit();
+    FetchCustomerDetails();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [token]);
+
   return (
     <CreditSideMainDrawer anchor="right" open={open} onClose={handleClose}>
-      {/* <Box sx={{ width: '100%', height: '100%', display: 'flex', flexDirection: 'column', position: 'relative' }}> */}
-      <CreditsHeader>
-        <TitleSerachBox>
-          <UINewTypography variant="h3" fontSize={30} color="text.secondary">
-            Add credits
-          </UINewTypography>
-        </TitleSerachBox>
-        <IconButton onClick={handleClose}>
-          <Close sx={{ color: 'text.secondary', height: 40, width: 40 }} />
-        </IconButton>
-      </CreditsHeader>
-      <CreditsContent>
-        <CurrentBalanceBox sx={{}}>
-          <CurrentBalanceInnerBox>
-            <CurrentBalanceTypography>Current Balance : </CurrentBalanceTypography>
-            <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
-              <Box component={'img'} src="/images/credits/coinwthIcon.png" alt="coin.png" width={26} height={26} />
-              <CurrentBalanceTypography>40</CurrentBalanceTypography>
-            </Box>
-          </CurrentBalanceInnerBox>
-        </CurrentBalanceBox>
-        <MainImageBox sx={{}}></MainImageBox>
-        <CreditListMainBox sx={{}}>bottom part</CreditListMainBox>
-      </CreditsContent>
-      {/* </Box> */}
+      {isLoading ? (
+        <CreditLoaderBox>
+          <CircularProgress />
+        </CreditLoaderBox>
+      ) : (
+        <>
+          <CreditsHeader>
+            <TitleSerachBox>
+              <UINewTypography variant="h3" fontSize={30} color="text.secondary">
+                <FormattedMessage id="Addcredits" />
+              </UINewTypography>
+            </TitleSerachBox>
+            <IconButton onClick={handleClose}>
+              <Close sx={{ color: 'text.secondary', height: 40, width: 40 }} />
+            </IconButton>
+          </CreditsHeader>
+          <CreditsContent>
+            <CurrentBalanceBox>
+              <CurrentBalanceTypography>
+                <FormattedMessage id="CurrentBalance" /> :
+              </CurrentBalanceTypography>
+              <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+                <Box component={'img'} src="/images/credits/coinwthIcon.png" alt="coin.png" width={26} height={26} />
+                <CurrentBalanceTypography>{balance?.toFixed(2)}</CurrentBalanceTypography>
+              </Box>
+            </CurrentBalanceBox>
+            <MainImageBox />
+            <CreditListMainBox>
+              {/* FREE CRDITS  */}
+              {customerDetails?.free_credits_claimed === 0 && (
+                <CreditListContainer
+                  sx={{
+                    background: 'linear-gradient(90deg, #FECD3D 11.5%, #FFF1C6 52%, #FF69C1 90%)',
+                    border: 'none'
+                  }}
+                  onClick={() => router.push('/profile')}
+                >
+                  <CreditInfoBox>
+                    <Box component={'img'} src="/images/credits/coinwthIcon.png" alt="coin.png" width={18} height={18} />
+                    <Box>
+                      <UINewTypography variant="buttonLargeMenu" color={'primary.200'}>
+                        20+10 <FormattedMessage id="Credits" />
+                      </UINewTypography>
+                    </Box>
+                  </CreditInfoBox>
+                  <CreditPriceBox>
+                    <Box component={'img'} src="/images/home/gitftsecond.png" alt="coin.png" width={24} height={29} />
+                    <CreditTypography color={'primary.200'}>
+                      <FormattedMessage id="FREE" />
+                    </CreditTypography>
+                  </CreditPriceBox>
+                </CreditListContainer>
+              )}
+
+              {/* NORMAL CRDITS  1*/}
+              <CreditListContainer onClick={() => handleCreditClick(creditsListing[0])}>
+                <CreditInfoBox>
+                  <Box component={'img'} src="/images/credits/coinwthIcon.png" alt="coin.png" width={18} height={18} />
+                  <Box>
+                    <UINewTypography variant="buttonLargeMenu" color={'white.main'}>
+                      {creditsListing[0]?.credits} <FormattedMessage id="Credits" />
+                    </UINewTypography>
+                  </Box>
+                </CreditInfoBox>
+                <CreditPriceBox>
+                  <CreditTypography color={'white.main'}>${creditsListing[0]?.amount}</CreditTypography>
+                </CreditPriceBox>
+              </CreditListContainer>
+
+              {/* NORMAL CRDITS  2*/}
+              <CreditListContainer onClick={() => handleCreditClick(creditsListing[1])}>
+                <CreditInfoBox>
+                  <Box component={'img'} src="/images/credits/coinwthIcon.png" alt="coin.png" width={18} height={18} />
+                  <Box>
+                    <UINewTypography variant="buttonLargeMenu" color={'white.main'}>
+                      {creditsListing[1]?.credits} <FormattedMessage id="Credits" />
+                    </UINewTypography>
+                  </Box>
+                </CreditInfoBox>
+                <CreditPriceBox>
+                  <CreditTypography color={'white.main'}>${creditsListing[1]?.amount}</CreditTypography>
+                </CreditPriceBox>
+              </CreditListContainer>
+
+              {/* MOST POPULAR CRDITS  1*/}
+              <CreditListContainer
+                sx={{
+                  background: 'linear-gradient(90deg, #FF68C0 0%, #9F1666 100%)',
+                  position: 'relative',
+                  border: 'none'
+                }}
+                onClick={() => handleCreditClick(creditsListing[2])}
+              >
+                <CreditPopularChip>
+                  <Box component={'img'} src="/images/credits/StarPink.svg" alt="coin.png" width={16} height={16} />
+                  <UINewTypography variant="bodySmallBold" color={'primary.400'}>
+                    <FormattedMessage id="MostPopular" />
+                  </UINewTypography>
+                </CreditPopularChip>
+                <CreditInfoBox>
+                  <Box component={'img'} src="/images/credits/coinwthIcon.png" alt="coin.png" width={18} height={18} />
+                  <Box>
+                    <UINewTypography variant="buttonLargeMenu" color={'white.main'}>
+                      {creditsListing[2]?.credits} <FormattedMessage id="Credits" />
+                    </UINewTypography>
+                  </Box>
+                </CreditInfoBox>
+                <CreditPriceBox>
+                  <CreditTypography color={'white.main'}>${creditsListing[2]?.amount}</CreditTypography>
+                </CreditPriceBox>
+              </CreditListContainer>
+
+              {/* NORMAL CRDITS  4*/}
+
+              {/* Best Value */}
+              <CreditListContainer
+                sx={{
+                  background: 'linear-gradient(90deg, #B88A4A 0%, #E0AA3E 31.5%, #E0AA3E 61.5%, #F9F295 100%)',
+                  position: 'relative',
+                  border: 'none'
+                }}
+                onClick={() => handleCreditClick(creditsListing[3])}
+              >
+                <CreditPopularChip>
+                  <Box component={'img'} src="/images/credits/dollar.svg" alt="coin.png" width={9} height={18} />
+                  <UINewTypography variant="bodySmallBold" color={'primary.400'}>
+                    <FormattedMessage id="BestValue" />
+                  </UINewTypography>
+                </CreditPopularChip>
+                <CreditInfoBox>
+                  <Box component={'img'} src="/images/credits/coinwthIcon.png" alt="coin.png" width={18} height={18} />
+                  <Box>
+                    <UINewTypography variant="buttonLargeMenu" color={'secondary.800'}>
+                      {creditsListing[3]?.credits} <FormattedMessage id="Credits" />
+                    </UINewTypography>
+                  </Box>
+                </CreditInfoBox>
+                <CreditPriceBox>
+                  <CreditTypography color={'secondary.800'}>$ {creditsListing[3]?.amount}</CreditTypography>
+                </CreditPriceBox>
+              </CreditListContainer>
+
+              {/* NORMAL CRDITS  5*/}
+              <CreditListContainer onClick={() => handleCreditClick(creditsListing[4])}>
+                <CreditInfoBox>
+                  <Box component={'img'} src="/images/credits/coinwthIcon.png" alt="coin.png" width={18} height={18} />
+                  <Box>
+                    <UINewTypography variant="buttonLargeMenu" color={'white.main'}>
+                      {creditsListing[4]?.credits} <FormattedMessage id="Credits" />
+                    </UINewTypography>
+                  </Box>
+                </CreditInfoBox>
+                <CreditPriceBox>
+                  <CreditTypography color={'white.main'}>${creditsListing[4]?.amount}</CreditTypography>
+                </CreditPriceBox>
+              </CreditListContainer>
+
+              {/* NORMAL CRDITS  6*/}
+              <CreditListContainer onClick={() => handleCreditClick(creditsListing[5])}>
+                <CreditInfoBox>
+                  <Box component={'img'} src="/images/credits/coinwthIcon.png" alt="coin.png" width={18} height={18} />
+                  <Box>
+                    <UINewTypography variant="buttonLargeMenu" color={'white.main'}>
+                      {creditsListing[5]?.credits} <FormattedMessage id="Credits" />
+                    </UINewTypography>
+                  </Box>
+                </CreditInfoBox>
+                <CreditPriceBox>
+                  <CreditTypography color={'white.main'}>${creditsListing[5]?.amount}</CreditTypography>
+                </CreditPriceBox>
+              </CreditListContainer>
+
+              {/* LIST END */}
+            </CreditListMainBox>
+          </CreditsContent>
+        </>
+      )}
     </CreditSideMainDrawer>
   );
 };
