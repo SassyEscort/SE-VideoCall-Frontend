ac2e574b4942d6ca233452417d54a27d9188200d
Mayur Bhagat <mayur.divtech@gmail.com>
2024-10-21 16:53:50 +0530

    ui style component remove

--- src/components/UIComponents/DragAndDropImage/DragAndDropMultipleImage.styled.tsx
+++ src/components/UIComponents/DragAndDropImage/DragAndDropMultipleImage.styled.tsx
@@ -18,62 +18,6 @@ export const DragAndDropMultipleImageCloseButton = styled(IconButton)(() => ({
   }
 }));
 
-export const DragAndDropMultipleImageEditButton = styled(IconButton)(() => ({
-  height: '22px',
-  width: '21px',
-  position: 'absolute',
-  right: '12px',
-  top: '12px',
-  zIndex: 100,
-
-  '& .MuiSvgIcon-root ': {
-    color: 'white',
-    width: '20px',
-    height: '20px'
-  }
-}));
-
-export const DragAndDropImageNoImageBox = styled(Box)(({ theme }) => ({
-  position: 'absolute',
-  top: 0,
-  width: '100%',
-  height: '100%',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  flexDirection: 'column',
-  cursor: 'pointer',
-  padding: theme.spacing(1)
-}));
-
-export const DragAndDropImageMainContainer = styled(Box)(({ theme }) => ({
-  padding: theme.spacing(2),
-  position: 'relative',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  width: '100%',
-  minHeight: '150px',
-  minWidth: '100px',
-  overflow: 'hidden',
-  '&:before': {
-    content: '""',
-    position: 'absolute',
-    border: '10px dashed',
-    top: '-9px',
-    bottom: '-9px',
-    left: '-9px',
-    right: '-9px',
-    borderRadius: '25px 0',
-    borderColor: theme.palette.secondary[800]
-  },
-  borderRadius: '16px 0px',
-  '&:hover': {
-    backgroundColor: theme.palette.primary[100]
-  },
-  cursor: 'pointer'
-}));
-
 export const DragAndDropContainer = styled(Box)<{
   isPDF: boolean;
   uploadedFileURL: string;
