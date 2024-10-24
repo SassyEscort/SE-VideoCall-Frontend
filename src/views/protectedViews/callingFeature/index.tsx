f188384dde817b02542c5fffeda83697b2cfcf29
Mayur Bhagat <mayur.divtech@gmail.com>
2024-10-24 15:26:03 +0530

    Revert ss changes from staging

--- src/views/protectedViews/callingFeature/index.tsx
+++ src/views/protectedViews/callingFeature/index.tsx
@@ -1,12 +1,9 @@
 'use client';
-import { useCallback, useEffect, useState } from 'react';
 import { useCallFeatureContext } from '../../../../context/CallFeatureContext';
 import RingingModel from '../videoCalling/RingingModel';
 import AnotherCallModel from '../videoCalling/AnotherCallModel';
 import OfflineModel from '../videoCalling/offlineModel';
-import { TokenIdType } from 'views/protectedModelViews/verification';
 import dynamic from 'next/dynamic';
-import { ScreenshotService } from 'services/screenshot/screenshot.service';
 
 const CometChatIncomingCall = dynamic(() => import('@cometchat/chat-uikit-react').then((mod) => mod.CometChatIncomingCall), { ssr: false });
 const CometChatOngoingCall = dynamic(() => import('@cometchat/chat-uikit-react').then((mod) => mod.CometChatOngoingCall), { ssr: false });
@@ -22,74 +19,8 @@ const CallFeature = () => {
     handleBusyClose,
     isLoading,
     isModelAvailable,
-    handleModelOfflineClose,
-    isModelJoin,
-    getToken,
-    callLogId
+    handleModelOfflineClose
   } = useCallFeatureContext();
-
-  const token: TokenIdType = getToken();
-
-  const [intervalDuration, setIntervalDuration] = useState<number | null>(null);
-  const [nextScreenshotTime, setNextScreenshotTime] = useState<number | null>(null);
-
-  const saveScreenshot = async () => {
-    const captureElement = document.querySelector('#cc-callscreen_ref') as HTMLElement;
-    if (captureElement) {
-      const html2canvas = (await import('html2canvas')).default;
-      const canvas = await html2canvas(captureElement);
-
-      canvas.toBlob(async (blob) => {
-        if (blob) {
-          const fileName = `${callLogId.toString()}_${Date.now()}`;
-          const formData = new FormData();
-          formData.append('call_log_id', callLogId.toString());
-          formData.append('screenshot', blob, fileName);
-          await ScreenshotService.uploadScreenShotImage(formData, token.token);
-        }
-      });
-    }
-  };
-
-  const fetchAndScheduleScreenshot = useCallback(async () => {
-    if (!isCallAccepted || !token?.token || !isModelJoin || !callLogId) return;
-
-    const durationRes = await ScreenshotService.fetchScreenShotDuration(token.token);
-    const intervalInSeconds = parseInt(durationRes?.data?.screenshot_interval_duration?.toString() || '0', 10);
-    if (intervalInSeconds && intervalInSeconds > 0) {
-      const intervalInMilliseconds = (intervalInSeconds - 1) * 1000;
-      setIntervalDuration(intervalInMilliseconds);
-      const currentTime = Date.now();
-      const nextTime = currentTime + intervalInMilliseconds;
-      setNextScreenshotTime(nextTime);
-    }
-  }, [token, isCallAccepted, isModelJoin, callLogId]);
-
-  const handleScreenshotInterval = useCallback(() => {
-    if (!nextScreenshotTime || !intervalDuration) return;
-
-    const currentTime = Date.now();
-    const timeUntilNextScreenshot = nextScreenshotTime - currentTime;
-
-    if (timeUntilNextScreenshot <= 0) {
-      saveScreenshot();
-      fetchAndScheduleScreenshot();
-    } else {
-      setTimeout(() => {
-        handleScreenshotInterval();
-      }, timeUntilNextScreenshot);
-    }
-  }, [nextScreenshotTime, intervalDuration, fetchAndScheduleScreenshot]);
-
-  useEffect(() => {
-    fetchAndScheduleScreenshot();
-  }, [fetchAndScheduleScreenshot]);
-
-  useEffect(() => {
-    if (nextScreenshotTime && intervalDuration) {
-      handleScreenshotInterval();
-    }
-  }, [nextScreenshotTime, intervalDuration, handleScreenshotInterval]);
 
   return (
     <>
