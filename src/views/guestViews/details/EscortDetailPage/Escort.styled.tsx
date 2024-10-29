100b76671943a3de60645a5c71b3369001a57ba7
Mayur Bhagat <mayur.divtech@gmail.com>
2024-10-29 11:16:38 +0530

    bio and language box bugs solve

--- src/views/guestViews/details/EscortDetailPage/Escort.styled.tsx
+++ src/views/guestViews/details/EscortDetailPage/Escort.styled.tsx
@@ -6,10 +6,11 @@ import { SwiperSlide } from 'swiper/react';
 
 export const DetailsMainBox = styled(Box)(() => ({
   width: '100%',
-  maxWidth: '517px',
+  minWidth: '517px',
   backgroundColor: '#1E0815',
   padding: '16px 32px 16px 32px',
-  borderRadius: '12px'
+  borderRadius: '12px',
+  height: '100%'
 }));
 
 export const DetailsChildBox = styled(Box)(({ theme }) => ({
