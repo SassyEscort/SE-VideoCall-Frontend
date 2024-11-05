458503266115b893970a5aa1a28746d8ec2425f1
dhruv-divtech <dhruv.divtech@gmail.com>
2024-11-05 17:09:55 +0530

    web cam dashboard

--- src/views/CamDashboardViews/index.tsx
+++ src/views/CamDashboardViews/index.tsx
@@ -1,10 +1,12 @@
 'use client';
 import { CamToCamDashboardBanner } from './camMainBanner';
+import { WebcamDashboard } from './webCamDashboard';
 
 export const CamToCamDashboard = () => {
   return (
     <>
       <CamToCamDashboardBanner />
+      <WebcamDashboard />
     </>
   );
 };
