ab16242db687c1b8389621a433de9b673216a290
karandivtech <karan.divtech@gmail.com>
2024-11-11 16:07:50 +0530

    chnage into after logged in header.

--- src/app/(camDashboard)/cam-to-cam/page.tsx
+++ src/app/(camDashboard)/cam-to-cam/page.tsx
@@ -1,7 +1,5 @@
 import { CamToCamDashboard } from 'views/CamDashboardViews';
 
-const Home = () => {
-  return <CamToCamDashboard />;
-};
+const Home = () => <CamToCamDashboard />;
 
 export default Home;
