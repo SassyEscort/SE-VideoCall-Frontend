7455c41bc902590918116bebf0dd78ac873516e0
dhruv-divtech <dhruv.divtech@gmail.com>
2024-11-12 13:38:42 +0530

    user page ui changes

--- src/views/admin/UsersPage/UpsertUser.tsx
+++ src/views/admin/UsersPage/UpsertUser.tsx
@@ -4,6 +4,7 @@ import MainLayout from '../layouts/AdminLayout/DashboardLayout';
 import Box from '@mui/material/Box';
 import {
   Button,
+  CircularProgress,
   FormControl,
   FormControlLabel,
   Grid,
@@ -38,6 +39,8 @@ const UpsertUser = () => {
   const [token, setToken] = useState<TokenIdType>({ id: 0, token: '' });
   const [userData, setUserData] = useState<getUserByIdData>();
   const [open, setOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
   const isCreatePage = usePathname().includes('update-permission');
 
   const handleClose = () => {
@@ -56,6 +59,7 @@ const UpsertUser = () => {
     });
 
   const handelFetchUserData = async (id: number) => {
+    setIsLoading(true);
     try {
       if (token.token) {
         const res = await adminUserServices.getUserById(id, token.token);
@@ -66,10 +70,12 @@ const UpsertUser = () => {
     } catch (error) {
       toast.error(ErrorMessage);
     }
+    setIsLoading(false);
   };
 
   const fetchModuleList = async () => {
     try {
+      setIsLoading(true);
       if (token.token) {
         const res = await adminUserServices.getModuleList(token.token);
         setModuleList(res.data);
@@ -77,6 +83,7 @@ const UpsertUser = () => {
     } catch (error) {
       toast.error(ErrorMessage);
     }
+    setIsLoading(false);
   };
 
   const handelUserRegistration = async (values: UserRegistrationParames) => {
@@ -128,124 +135,145 @@ const UpsertUser = () => {
 
   return (
     <MainLayout>
-      <Formik
-        initialValues={{
-          email: userData?.email || '',
-          password: userData?.password || '',
-          permission: userId
-            ? userData?.module_permissions.map((user) => ({ module_id: user.id, permission: user.permission || 'None' }))
-            : moduleList.map((module) => ({ module_id: module.id, permission: 'None' }))
-        }}
-        validationSchema={validationSchema}
-        enableReinitialize
-        onSubmit={(values) => {
-          if (values) {
-            // handelUserRegistration(values);
-            const userRegistrationParams: UserRegistrationParames = {
-              ...values,
-              permissions: values.permission || []
-            };
+      {isLoading ? (
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            p: 2
+          }}
+        >
+          <CircularProgress />
+        </Box>
+      ) : (
+        <Formik
+          initialValues={{
+            email: userData?.email || '',
+            password: userData?.password || '',
+            permission: userId
+              ? userData?.module_permissions.map((user) => ({ module_id: user.id, permission: user.permission || 'None' }))
+              : moduleList.map((module) => ({ module_id: module.id, permission: 'None' }))
+          }}
+          validationSchema={validationSchema}
+          enableReinitialize
+          onSubmit={(values) => {
+            if (values) {
+              // handelUserRegistration(values);
+              const userRegistrationParams: UserRegistrationParames = {
+                ...values,
+                permissions: values.permission || []
+              };
 
-            if (userId) {
-              handelUpdateUser(Number(userId), userRegistrationParams);
-            } else handelUserRegistration(userRegistrationParams);
-          }
-        }}
-      >
-        {({ values, errors, touched, handleChange, handleBlur, handleSubmit, setFieldValue }) => (
-          <Box component="form" onSubmit={handleSubmit}>
-            <Grid container>
-              <Grid item xs={12} sm={6}>
-                <Typography variant="h6" gutterBottom>
-                  Email
-                </Typography>
-                <TextField
-                  name="email"
-                  type="string"
-                  disabled={userId ? true : false}
-                  value={values.email}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  error={touched.email && Boolean(errors.email)}
-                  helperText={touched.email && errors.email}
-                  sx={{ width: '100%', maxWidth: '500px' }}
-                />
-              </Grid>
+              if (userId) {
+                handelUpdateUser(Number(userId), userRegistrationParams);
+              } else handelUserRegistration(userRegistrationParams);
+            }
+          }}
+        >
+          {({ values, errors, touched, handleChange, handleBlur, handleSubmit, setFieldValue }) => (
+            <Box
+              component="form"
+              onSubmit={handleSubmit}
+              sx={{ display: 'flex', flexDirection: 'column', gap: '20px', alignItems: 'center' }}
+            >
+              <Grid container>
+                <Grid item xs={12} sm={6}>
+                  <Typography variant="h6" gutterBottom>
+                    Email
+                  </Typography>
+                  <TextField
+                    name="email"
+                    type="string"
+                    disabled={userId ? true : false}
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.email && Boolean(errors.email)}
+                    helperText={touched.email && errors.email}
+                    sx={{ width: '100%', maxWidth: '500px' }}
+                  />
+                </Grid>
 
-              <Grid item xs={12} sm={6}>
-                <Typography variant="h6" gutterBottom>
-                  Password
-                </Typography>
+                <Grid item xs={12} sm={6}>
+                  <Typography variant="h6" gutterBottom>
+                    Password
+                  </Typography>
 
-                <TextField
-                  name="password"
-                  type="password"
-                  disabled={userId ? true : false}
-                  value={values.password}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  error={touched.password && Boolean(errors.password)}
-                  helperText={touched.password && errors.password}
-                  sx={{ width: '100%', maxWidth: '500px' }}
-                  InputProps={{
-                    endAdornment: (
-                      <InputAdornment position="end">
-                        <IconButton onClick={() => setOpen(true)}>
-                          <EditIcon />
-                        </IconButton>
-                      </InputAdornment>
-                    )
-                  }}
-                />
+                  <TextField
+                    name="password"
+                    type="password"
+                    disabled={userId ? true : false}
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={touched.password && Boolean(errors.password)}
+                    helperText={touched.password && errors.password}
+                    sx={{ width: '100%', maxWidth: '500px' }}
+                    InputProps={{
+                      endAdornment: (
+                        <InputAdornment position="end">
+                          <IconButton onClick={() => setOpen(true)}>
+                            <EditIcon />
+                          </IconButton>
+                        </InputAdornment>
+                      )
+                    }}
+                  />
+                </Grid>
               </Grid>
-            </Grid>
-            <Grid container>
-              <Grid item xs={6}>
-                <Typography variant="h6">Modules</Typography>
-              </Grid>
-              <Grid item xs={6}>
-                <Typography variant="h6">permissions</Typography>
-              </Grid>
-              {moduleList &&
-                moduleList.length > 0 &&
-                moduleList.map((module, index) => (
-                  <Grid container key={index}>
-                    <Grid item xs={6}>
-                      <Typography variant="h6">{module.module_name}</Typography>
+              <Grid container rowGap={1}>
+                <Grid item xs={6}>
+                  <Typography variant="h6">Modules</Typography>
+                </Grid>
+                <Grid item xs={6}>
+                  <Typography variant="h6">Permissions</Typography>
+                </Grid>
+                {moduleList &&
+                  moduleList.length > 0 &&
+                  moduleList.map((module, index) => (
+                    <Grid container key={index}>
+                      <Grid item xs={3} sm={6}>
+                        <Typography variant="h6">{module.module_name}</Typography>
+                      </Grid>
+                      <Grid item xs={9} sm={6}>
+                        <FormControl>
+                          <RadioGroup
+                            row
+                            value={values.permission ? values.permission[index]?.permission : 'None'}
+                            onChange={(e) => {
+                              setFieldValue(`permission[${index}].module_id`, module.id);
+                              setFieldValue(`permission[${index}].permission`, e.target.value);
+                            }}
+                            defaultValue="None"
+                          >
+                            <FormControlLabel value="None" control={<Radio />} label="None" sx={{ mr: { xs: 1, sm: 4 } }} />
+                            <FormControlLabel value="Read" control={<Radio />} label="Read" sx={{ mr: { xs: 1, sm: 4 } }} />
+                            <FormControlLabel value="Update" control={<Radio />} label="Read & Update" sx={{ mr: { xs: 1, sm: 4 } }} />
+                          </RadioGroup>
+                        </FormControl>
+                      </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={6}>
-                      <FormControl>
-                        <RadioGroup
-                          row
-                          value={values.permission ? values.permission[index]?.permission : 'None'}
-                          onChange={(e) => {
-                            setFieldValue(`permission[${index}].module_id`, module.id);
-                            setFieldValue(`permission[${index}].permission`, e.target.value);
-                          }}
-                          defaultValue="None"
-                        >
-                          <FormControlLabel value="None" control={<Radio />} label="None" />
-                          <FormControlLabel value="Read" control={<Radio />} label="Read" />
-                          <FormControlLabel value="Update" control={<Radio />} label="Read & Update" />
-                        </RadioGroup>
-                      </FormControl>
-                    </Grid>
-                  </Grid>
-                ))}
-            </Grid>
+                  ))}
 
-            {userId ? (
-              <Button size="large" variant="contained" type="submit">
-                Update User
-              </Button>
-            ) : (
-              <Button size="large" variant="contained" type="submit">
-                Create User
-              </Button>
-            )}
-          </Box>
-        )}
-      </Formik>
+                <Grid item xs={11}>
+                  <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                    {userId ? (
+                      <Button size="large" variant="contained" type="submit" sx={{ width: 'fit-content' }}>
+                        Update User
+                      </Button>
+                    ) : (
+                      <Button size="large" variant="contained" type="submit" sx={{ width: 'fit-content' }}>
+                        Create User
+                      </Button>
+                    )}
+                  </Box>
+                </Grid>
+              </Grid>
+            </Box>
+          )}
+        </Formik>
+      )}
       <UpdateUserPassword open={open} onClose={handleClose} userId={Number(userId)} />
     </MainLayout>
   );
