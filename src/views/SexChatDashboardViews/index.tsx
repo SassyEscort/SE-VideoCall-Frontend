2f9d855adf9979f3036a6e7cbacb5d3dbbad860b
dhruv-divtech <dhruv.divtech@gmail.com>
2024-11-22 18:55:56 +0530

    resolve confilts

--- src/views/SexChatDashboardViews/index.tsx
+++ src/views/SexChatDashboardViews/index.tsx
@@ -7,9 +7,9 @@ import ExclusiveSexChatDashboard from './ExclusiveSexChatDashboard';
 import ReadyOneOnOneComponent from './readyOneOnOne';
 import MeetRealPeople from './meetRealPeopleBanner';
 import HomeContainer from 'views/guestViews/homePage';
-import { useAuthContext } from '../../../context/AuthContext';
 import LiveSexChatDashboard from './LiveSexChat';
 import HowSexVideoChatWorks from './HowSexVideoChatWorks';
+import { useAuthContext } from 'contexts/AuthContext';
 
 export const SexChatDashBoard = () => {
   const { isCustomer } = useAuthContext();
