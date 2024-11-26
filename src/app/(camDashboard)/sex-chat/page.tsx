9ca31e2dde75f93ff0148b5adbab261e41fda03e
dhruv-divtech <dhruv.divtech@gmail.com>
2024-11-26 18:13:14 +0530

    add meta data

--- src/app/(camDashboard)/sex-chat/page.tsx
+++ src/app/(camDashboard)/sex-chat/page.tsx
@@ -1,4 +1,11 @@
+import { SEO_DATA_SEX_CHAT } from 'constants/seoConstants';
+import { Metadata } from 'next';
 import { SexChatDashBoard } from 'views/SexChatDashboardViews';
+
+export const metadata: Metadata = {
+  title: SEO_DATA_SEX_CHAT.TITLE,
+  description: SEO_DATA_SEX_CHAT.DESCRIPTION
+};
 
 const Home = () => <SexChatDashBoard />;
 
