203e7e35c02fd52ab1432ffd2967883f85114399
dhruv-divtech <dhruv.divtech@gmail.com>
2024-11-26 18:04:05 +0530

    seo data update

--- src/app/(camDashboard)/chat-with-girls/page.tsx
+++ src/app/(camDashboard)/chat-with-girls/page.tsx
@@ -1,4 +1,11 @@
+import { SEO_DATA_CHAT_WITH_GIRLS } from 'constants/seoConstants';
+import { Metadata } from 'next';
 import { ChatToGirlDashBoard } from 'views/ChatToGirlDashBoardViews';
+
+export const metadata: Metadata = {
+  title: SEO_DATA_CHAT_WITH_GIRLS.TITLE,
+  description: SEO_DATA_CHAT_WITH_GIRLS.DESCRIPTION
+};
 
 const Home = () => <ChatToGirlDashBoard />;
 
