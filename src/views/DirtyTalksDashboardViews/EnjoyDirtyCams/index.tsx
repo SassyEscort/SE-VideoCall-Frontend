810a28d00fa6ae30381522e9a56aebd8d8af4368
Mayur Bhagat <mayur.divtech@gmail.com>
2024-12-03 17:26:42 +0530

    resolve conflicts

--- src/views/DirtyTalksDashboardViews/EnjoyDirtyCams/index.tsx
+++ src/views/DirtyTalksDashboardViews/EnjoyDirtyCams/index.tsx
@@ -131,9 +131,9 @@ const EnjoyDirtyCams = ({ isCustomer }: { isCustomer: boolean }) => {
           onFogotPasswordLinkOpen={handleResetPasswordLinkOpen}
           isFreeCreditAvailable={isFreeCreditAvailable}
           handleFreeCreditSignupOpen={handleFreeCreditSignupOpen}
-          handleLoginOpen={handleLoginOpen}
-          freeSignupOpen={freeSignupOpen}
-          handleFreeCreditSignupClose={handleFreeCreditSignupClose}
+          //   handleLoginOpen={handleLoginOpen}
+          //   freeSignupOpen={freeSignupOpen}
+          //   handleFreeCreditSignupClose={handleFreeCreditSignupClose}
           image="/images/auth/auth-model1.webp"
         />
       </UIStyledDialog>
