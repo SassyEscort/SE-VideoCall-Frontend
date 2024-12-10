d3655c5b9370d01fb54cab4f7e4444a6cf08d547
Mayur Bhagat <mayur.divtech@gmail.com>
2024-12-10 12:54:04 +0530

    add meta tage,title and description

--- src/app/(camDashboard)/nude-chat/page.tsx
+++ src/app/(camDashboard)/nude-chat/page.tsx
@@ -1,12 +1,12 @@
-// import { SEO_DATA_SEX_CHAT } from 'constants/seoConstants';
-// import { Metadata } from 'next';
+import { SEO_DATA_NUDE_LIVE_VIDEO_CHAT } from 'constants/seoConstants';
+import { Metadata } from 'next';
 import { KeyPairAndUndefined } from 'types/KeyPair';
 import { NudeChatDashboard } from 'views/NudeChatDashboardView';
 
-// export const metadata: Metadata = {
-//   title: SEO_DATA_SEX_CHAT.TITLE,
-//   description: SEO_DATA_SEX_CHAT.DESCRIPTION
-// };
+export const metadata: Metadata = {
+  title: SEO_DATA_NUDE_LIVE_VIDEO_CHAT.TITLE,
+  description: SEO_DATA_NUDE_LIVE_VIDEO_CHAT.DESCRIPTION
+};
 
 const Home = ({ searchParams }: { searchParams: KeyPairAndUndefined }) => <NudeChatDashboard searchParams={searchParams} />;
 
