a1e154cca3b94472bfbbf0243b9ea36fd8d515da
Mayur Bhagat <mayur.divtech@gmail.com>
2024-12-11 16:11:49 +0530

    Initial commit

---
+++ src/views/guestViews/abTestComponent/loginPage/user/view1/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const View1 = ({ group }: { group: string }) => {
+  return <div>View1 {group}</div>;
+};
+
+export default View1;
---
+++ src/views/guestViews/abTestComponent/loginPage/user/view2/index.tsx
@@ -0,0 +1,9 @@
+const View2 = ({ group }: { group: string }) => {
+  return (
+    <div>
+      <span>View2 {group}</span>
+    </div>
+  );
+};
+
+export default View2;
