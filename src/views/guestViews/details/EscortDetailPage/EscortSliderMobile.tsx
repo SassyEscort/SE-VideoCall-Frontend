04d36fba52e3c6033975001608260a0f257be94b
dhruv-divtech <dhruv.divtech@gmail.com>
2024-12-17 18:49:45 +0530

    hot fix chat changes

--- src/views/guestViews/details/EscortDetailPage/EscortSliderMobile.tsx
+++ src/views/guestViews/details/EscortDetailPage/EscortSliderMobile.tsx
@@ -32,7 +32,7 @@ import { sortExistingPhotos } from 'utils/photoUtils';
 import { ModelDetailsResponse } from 'views/protectedModelViews/verification/verificationTypes';
 import EscortSwiperPhotoContainerSide from './EscortSwiperPhotoContainerSide';
 import { gaEventTrigger } from 'utils/analytics';
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
 import GuestFreeCreditsSignup from 'views/auth/guestFreeCreditsSignup';
 import useMediaQuery from '@mui/material/useMediaQuery';
 import dynamic from 'next/dynamic';
@@ -78,7 +78,7 @@ const EscortSliderMobile = ({
   const sortedWorkerPhotos = workerPhotos.sort(sortExistingPhotos);
 
   const path = usePathname();
-  // const router = useRouter();
+  const router = useRouter();
   const userName = path.split('/')[2];
   const customerData = JSON.parse(user || '{}');
 
@@ -89,7 +89,7 @@ const EscortSliderMobile = ({
     model_username: userName
   };
 
-  // const handleStartChatClick = () => router.push(`/chat/${userName}`);
+  const handleStartChatClick = () => router.push(`/chat/${userName}`);
 
   const handleSignupOpen = () => {
     setIsOpenLogin(false);
@@ -244,7 +244,7 @@ const EscortSliderMobile = ({
           </Box>
         </StyleButtonShadowV2>
         <ActivityButtonBox>
-          {/* <StyleButtonShadowV2
+          <StyleButtonShadowV2
             loading={isLoading}
             onClick={isCustomer ? handleStartChatClick : handleLoginOpen}
             sx={{
@@ -265,7 +265,7 @@ const EscortSliderMobile = ({
                 <FormattedMessage id="StartChat" />
               </UINewTypography>
             </Box>
-          </StyleButtonShadowV2> */}
+          </StyleButtonShadowV2>
           <Box sx={{ width: '100%', height: '100%' }}>
             <UIStyledShadowButtonLike
               sx={{
