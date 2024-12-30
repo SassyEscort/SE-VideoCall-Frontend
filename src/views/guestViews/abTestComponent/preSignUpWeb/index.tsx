d83e98b60c580f79600e92f6609f06b7aa29fa75
Mayur Bhagat <mayur.divtech@gmail.com>
2024-12-30 18:05:59 +0530

    new pre signup web

---
+++ src/views/guestViews/abTestComponent/preSignUpWeb/index.tsx
@@ -0,0 +1,48 @@
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import UINewTypography from 'components/UIComponents/UINewTypography';
+import React from 'react';
+
+const PreSignUpWeb = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 4,
+        width: '100%',
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center',
+        textAlign: 'center'
+      }}
+    >
+      <Box sx={{ display: 'flex', flexDirection: 'column', gap: 1 }}>
+        <UINewTypography sx={{ fontSize: '100px', fontWeight: 900, lineHeight: '120px', color: 'white.main' }}>
+          Are You Ready to Get <span style={{ color: '#79E028' }}>Naughty?</span>
+        </UINewTypography>
+        <UINewTypography sx={{ fontSize: '24px', fontWeight: 400, lineHeight: '32px', color: 'white.main' }}>
+          Unleash your wildest desires with models who are here to tease, please, and make it all about you.
+        </UINewTypography>
+      </Box>
+
+      <Button
+        variant="contained"
+        sx={{
+          borderRadius: '100px',
+          width: '400px',
+          height: '72px',
+          gap: 1
+        }}
+      >
+        <Box component="img" src="/images/icons/new-video-call-icon.svg" alt="video-call" height={32} width={32} />
+
+        <UINewTypography sx={{ fontSize: '24px', fontWeight: 800, lineHeight: '32px', color: 'white.main' }}>
+          Start Free Video Chat
+        </UINewTypography>
+      </Button>
+    </Box>
+  );
+};
+
+export default PreSignUpWeb;
